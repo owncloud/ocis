70c496d830acfd92346689053b6a5a5f7ba575e3
A.Unger <zyxancf@gmail.com>
2019-12-17 10:01:04 +0100

    remove micro gateway service definition

--- pkg/command/server.go
+++ pkg/command/server.go
@@ -128,9 +128,6 @@ func Server(cfg *config.Config) cli.Command {
 
 			defer cancel()
 
-			// register a micro gateway
-			// mapi.MicroGateway(ctx, cancel, &gr, cfg)
-
 			for _, fn := range register.Handlers {
 				fn(ctx, cancel, &gr, cfg)
 			}
