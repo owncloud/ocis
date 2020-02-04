9dda684cf282d595c20195cb642e297921b34b02
A.Unger <zyxancf@gmail.com>
2020-02-04 12:39:29 +0100

    update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,21 @@
 // Package config should be moved to internal
 package config
 
-// Config captures ocis-accounts configuration parameters
+// Server configures a server.
+type Server struct {
+	Name      string
+	Namespace string
+	Address   string
+}
+
+// Config merges all Account config parameters.
 type Config struct {
 	MountPath string
 	Manager   string
+	Server    Server
 }
 
-// New returns a new config
+// New returns a new config.
 func New() *Config {
 	return &Config{}
 }
