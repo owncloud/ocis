868db44531b3891c9bb86a3a8f96845b4f913598
A.Unger <zyxancf@gmail.com>
2020-03-06 16:28:47 +0100

    add todo comment: update micro/cli to support StringSlice flags destination

--- pkg/command/server.go
+++ pkg/command/server.go
@@ -35,6 +35,7 @@ func Server(cfg *config.Config) *cli.Command {
 			}
 
 			// StringSliceFlag doesn't support Destination
+			// UPDATE Destination on string flags supported. Wait for https://github.com/urfave/cli/pull/1078 to get to micro/cli
 			if len(c.StringSlice("trusted-proxy")) > 0 {
 				cfg.Konnectd.TrustedProxy = c.StringSlice("trusted-proxy")
 			}
@@ -82,6 +83,10 @@ func Server(cfg *config.Config) *cli.Command {
 							ServiceName:       cfg.Tracing.Service,
 						},
 					)
+
+					logger.Info().
+						Str("collector", cfg.Tracing.Collector).
+						Msg("Trace collector added")
 
 					if err != nil {
 						logger.Error().
