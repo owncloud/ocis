739d226d0981b7fc54e7c809afdcb0e69594cd87
A.Unger <zyxancf@gmail.com>
2020-03-13 10:52:42 +0100

    add embedded-config flag, override config.File if present

--- pkg/flagset/flagset.go
+++ pkg/flagset/flagset.go
@@ -69,6 +69,12 @@ func ServerWithConfig(cfg *config.Config) []cli.Flag {
 			Destination: &cfg.Tracing.Type,
 		},
 		&cli.StringFlag{
+			Name:        "embedded-config",
+			Usage:       "Provided config file while running on single oCIS binary mode.",
+			EnvVars:     []string{"PROXY_EMBEDDED_CONFIG"},
+			Destination: &cfg.File,
+		},
+		&cli.StringFlag{
 			Name:        "tracing-endpoint",
 			Value:       "",
 			Usage:       "Endpoint for the agent",
