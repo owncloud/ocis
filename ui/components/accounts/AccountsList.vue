48d28cf7dd8e34373369461cbc6fb49ee15012ac
Benedikt Kulmann <benedikt@kulmann.biz>
2020-07-01 09:17:17 +0200

    Add more information to accounts list

--- ui/components/accounts/AccountsList.vue
+++ ui/components/accounts/AccountsList.vue
@@ -4,17 +4,28 @@
       <oc-table-group>
         <oc-table-row>
           <oc-table-cell shrink type="head" />
-          <oc-table-cell type="head" v-text="$gettext('Name')" />
+          <oc-table-cell type="head" v-text="$gettext('Username')" />
+          <oc-table-cell type="head" v-text="$gettext('Display name')" />
           <oc-table-cell type="head" v-text="$gettext('Email')" />
+          <oc-table-cell shrink type="head" v-text="$gettext('Uid number')" />
+          <oc-table-cell shrink type="head" v-text="$gettext('Gid number')" />
+          <oc-table-cell shrink type="head" v-text="$gettext('Enabled')" />
         </oc-table-row>
       </oc-table-group>
       <oc-table-group>
         <oc-table-row v-for="account in accounts" :key="`account-list-row-${account.id}`">
           <oc-table-cell>
-            <oc-avatar :userName="account.preferredName" />
+            <oc-avatar :userName="account.displayName || account.preferredName" :width="35" />
           </oc-table-cell>
           <oc-table-cell v-text="account.preferredName" />
+          <oc-table-cell v-text="account.displayName || '-'" />
           <oc-table-cell v-text="account.mail" />
+          <oc-table-cell v-text="account.uidNumber || '-'" />
+          <oc-table-cell v-text="account.gidNumber || '-'" />
+          <oc-table-cell class="uk-text-center">
+            <oc-icon v-if="account.accountEnabled" name="ready" variation="success" :aria-label="$gettext('Account is enabled')" />
+            <oc-icon v-else name="deprecated" variation="danger" :aria-label="$gettext('Account is disabled')" />
+          </oc-table-cell>
         </oc-table-row>
       </oc-table-group>
     </oc-table>
