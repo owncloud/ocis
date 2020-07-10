523f704034348dac2dcf3083c7ec01afbf7ed883
Jörn Friedrich Dreyer <jfd@butonic.de>
2020-07-10 13:26:10 +0200

    document bleve type
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- pkg/proto/v0/bleve.go
+++ pkg/proto/v0/bleve.go
@@ -1,13 +1,12 @@
 package proto
 
-// Bleve uses a private bleveClassifier interface to determine the type of a struct
-// see https://github.com/blevesearch/bleve/blob/master/mapping/mapping.go#L32-L38
-
+// BleveAccount wraps the generated Account and adds a bleve type that is used to distinguish documents in the index
 type BleveAccount struct {
 	Account
 	BleveType string `json:"bleve_type"`
 }
 
+// BleveGroup wraps the generated Group and adds a bleve type that is used to distinguish documents in the index
 type BleveGroup struct {
 	Group
 	BleveType string `json:"bleve_type"`
