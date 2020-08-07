6bf248cc99d6d6232947e3f5a60964d57bc5b70b
Dipak Acharya <dpakach@gmail.com>
2020-08-07 09:24:59 +0545

    add echo for debug

--- tests/record_file_descriptor.sh
+++ tests/record_file_descriptor.sh
@@ -7,8 +7,9 @@ CURRENT_FILE_DESCRIPTOR_STORE=${CURRENT_FILE_DESCRIPTOR_STORE:-"final.txt"}
 
 for i in `ps xao pid,cmd | grep ocis | grep -v grep`
 do 
-  # echo $i
+  echo $i
   trimmed=$(echo $i | xargs)
+  echo $trimmed
   PID=$(echo $trimmed | cut -d" " -f 1)
   SERVICE=$(echo $trimmed | cut -d" " -f 3)
   COUNT_DESCRIPTORS=`lsof -p $PID 2> /dev/null | wc -l`
