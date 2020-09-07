c2e8cd3d9f9924fffdb78a65e137d3e6e13ce1d9
Dipak Acharya <dpakach@gmail.com>
2020-09-07 09:40:17 +0545

    refactor scripts

--- tests/spawn_eos.sh
+++ tests/spawn_eos.sh
@@ -2,20 +2,20 @@
 ME=$DRONE_HCLOUD_USER
 SERVER_NAME=droneci-eos-test-${DRONE_COMMIT_ID}-${RUN_PART}
 
-# setup ssh keys for hcloud
-ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
-hcloud ssh-key --name drone-%s-%s --public-key-from-file /root/.ssh/id_rsa.pub
+# # setup ssh keys for hcloud
+# ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+# hcloud ssh-key create --name drone-${DRONE_COMMIT_ID}-${RUN_PART} --public-key-from-file /root/.ssh/id_rsa.pub
 
-# Create a new machine on hcloud for eos
-hcloud server create --type cx21 --image ubuntu-20.04 --ssh-key $SERVER_NAME --name $SERVER_NAME --label owner=$ME --label for=test --label from=eos-compose
-# time for the server to start up
-sleep 15
+# # Create a new machine on hcloud for eos
+# hcloud server create --type cx21 --image ubuntu-20.04 --ssh-key $SERVER_NAME --name $SERVER_NAME --label owner=$ME --label for=test --label from=eos-compose
+# # time for the server to start up
+# sleep 15
 
-IPADDR=$(hcloud server ip $SERVER_NAME)
-export IPADDR=$IPADDR
-export TEST_SERVER_URL=https://${IPADDR}:9200
+# IPADDR=$(hcloud server ip $SERVER_NAME)
+# export IPADDR=$IPADDR
+# export TEST_SERVER_URL=https://${IPADDR}:9200
 
-ssh -o StrictHostKeyChecking=no root@$IPADDR
+# ssh -o StrictHostKeyChecking=no root@$IPADDR
 
 
 # Setup system and clone ocis
