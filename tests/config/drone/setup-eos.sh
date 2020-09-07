fbe8324d85ed3823dc99e7e28066522f44b5a9b0
Dipak Acharya <dpakach@gmail.com>
2020-09-07 19:36:46 +0545

    fix docker-compose file

--- tests/config/drone/setup-eos.sh
+++ tests/config/drone/setup-eos.sh
@@ -7,17 +7,17 @@ do
     sleep 5
 done
 
-docker-compose -f ./docker-compose-eos-ci.yml exec -d ocis /start-ldap
+docker-compose exec -d ocis /start-ldap
 
 # time for ldap service to starup within ocis container
 sleep 5
 
 # Configure ocis
-docker-compose -f ./docker-compose-eos-ci.yml exec ocis id einstein
-docker-compose -f ./docker-compose-eos-ci.yml exec ocis /ocis/bin/ocis kill reva-users
-docker-compose -f ./docker-compose-eos-ci.yml exec ocis /ocis/bin/ocis run reva-users
-docker-compose -f ./docker-compose-eos-ci.yml exec ocis /ocis/bin/ocis kill reva-storage-home
-docker-compose -f ./docker-compose-eos-ci.yml exec -e REVA_STORAGE_HOME_DRIVER=eoshome -e REVA_STORAGE_HOME_MOUNT_ID=1284d238-aa92-42ce-bdc4-0b0000009158 ocis ./bin/ocis run reva-storage-home
-docker-compose -f ./docker-compose-eos-ci.yml exec ocis /ocis/bin/ocis kill reva-storage-home-data
-docker-compose -f ./docker-compose-eos-ci.yml exec -e REVA_STORAGE_HOME_DATA_DRIVER=eoshome ocis ./bin/ocis run reva-storage-home-data
+docker-compose exec ocis id einstein
+docker-compose exec ocis /ocis/bin/ocis kill reva-users
+docker-compose exec ocis /ocis/bin/ocis run reva-users
+docker-compose exec ocis /ocis/bin/ocis kill reva-storage-home
+docker-compose exec -e REVA_STORAGE_HOME_DRIVER=eoshome -e REVA_STORAGE_HOME_MOUNT_ID=1284d238-aa92-42ce-bdc4-0b0000009158 ocis ./bin/ocis run reva-storage-home
+docker-compose exec ocis /ocis/bin/ocis kill reva-storage-home-data
+docker-compose exec -e REVA_STORAGE_HOME_DATA_DRIVER=eoshome ocis ./bin/ocis run reva-storage-home-data
 
