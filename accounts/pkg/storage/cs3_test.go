329688be49a5c213f1284e138b351e1bd9810183
Ilja Neumann <node512@gmail.com>
2020-09-29 12:58:12 +0200

    - Implemented delete + some test

--- accounts/pkg/storage/cs3_test.go
+++ accounts/pkg/storage/cs3_test.go
@@ -1,7 +1,13 @@
 package storage
 
-/*
-func TestFoo(t *testing.T) {
+import (
+	"context"
+	"github.com/owncloud/ocis/accounts/pkg/proto/v0"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCS3Repo_WriteAccount(t *testing.T) {
 	r, err := NewCS3Repo("fooo")
 	assert.NoError(t, err)
 
@@ -15,4 +21,39 @@ func TestFoo(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-*/
+func TestCS3Repo_LoadAccount(t *testing.T) {
+	r, err := NewCS3Repo("fooo")
+	assert.NoError(t, err)
+
+	err = r.WriteAccount(context.Background(), &proto.Account{
+		Id:             "fefef-egegweg-gegeg",
+		AccountEnabled: true,
+		DisplayName:    "Mike Jones",
+		Mail:           "mike@example.com",
+	})
+
+	acc := &proto.Account{}
+	err = r.LoadAccount(context.Background(), "fefef-egegweg-gegeg", acc)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "fefef-egegweg-gegeg", acc.Id)
+	assert.Equal(t, "Mike Jones", acc.DisplayName)
+	assert.Equal(t, "mike@example.com", acc.Mail)
+}
+
+func TestCS3Repo_DeleteAccount(t *testing.T) {
+	r, err := NewCS3Repo("fooo")
+	assert.NoError(t, err)
+
+	err = r.WriteAccount(context.Background(), &proto.Account{
+		Id:             "delete-me-id",
+		AccountEnabled: true,
+		DisplayName:    "Mike Jones",
+		Mail:           "mike@example.com",
+	})
+
+	err = r.DeleteAccount(context.Background(), "delete-me-id")
+
+	assert.NoError(t, err)
+
+}
