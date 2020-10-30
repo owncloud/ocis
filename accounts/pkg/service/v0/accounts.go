494f9aca88b82c4fe9c87fe36183fa907197532b
Jörn Friedrich Dreyer <jfd@butonic.de>
2020-10-30 17:09:55 +0100

    enforce permissions
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- accounts/pkg/service/v0/accounts.go
+++ accounts/pkg/service/v0/accounts.go
@@ -65,12 +65,7 @@ func (s Service) hasAccountManagementPermissions(ctx context.Context) bool {
 	// get roles from context
 	roleIDs, ok := roles.ReadRoleIDsFromContext(ctx)
 	if !ok {
-		/**
-		 * FIXME: with this we are skipping permission checks on all requests that are coming in without roleIDs in the
-		 * metadata context. This is a huge security impairment, as that's the case not only for grpc requests but also
-		 * for unauthenticated http requests and http requests coming in without hitting the ocis-proxy first.
-		 */
-		return true
+		return false
 	}
 
 	// check if permission is present in roles of the authenticated account
