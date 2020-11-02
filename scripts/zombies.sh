e4a6aeb453ee9f948966765d5bf362eb4641a353
Jörn Friedrich Dreyer <jfd@butonic.de>
2020-11-02 15:40:37 +0100

    comment unused DIR
    only needed for the ocis cli command

--- scripts/zombies.sh
+++ scripts/zombies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 [[ -z "$OFFSET" ]] && OFFSET=1
 [[ -z "$COUNT" ]] && "$((COUNT = 1000))"
