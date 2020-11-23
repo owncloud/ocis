005825bbc9a3fcfa90ea7bed9b1e79241261617e
Benedikt Kulmann <benedikt@kulmann.biz>
2020-11-23 13:06:05 +0100

    Set proper expires header

--- ocis-pkg/middleware/static.go
+++ ocis-pkg/middleware/static.go
@@ -24,6 +24,7 @@ func Static(root string, fs http.FileSystem, ttl int) func(http.Handler) http.Ha
 
 	// we don't have a last modification date of the static assets, so we use the service start date
 	lastModified := time.Now().UTC().Format(http.TimeFormat)
+	expires := time.Now().Add(time.Second * time.Duration(ttl)).UTC().Format(http.TimeFormat)
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +32,8 @@ func Static(root string, fs http.FileSystem, ttl int) func(http.Handler) http.Ha
 				next.ServeHTTP(w, r)
 			} else {
 				w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%s", strconv.Itoa(ttl)))
+				w.Header().Set("Expires", expires)
 				w.Header().Set("Last-Modified", lastModified)
-				w.Header().Del("Expires")
 				static.ServeHTTP(w, r)
 			}
 		})
