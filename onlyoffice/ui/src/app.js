6522a41b267ec4b0d8bba36675f8b9f9de9d5fa9
Lukas Hirt <lukashirt@mail.com>
2020-11-24 12:47:48 +0100

    Use extension config

--- onlyoffice/ui/src/app.js
+++ onlyoffice/ui/src/app.js
@@ -1,37 +1,37 @@
 const appInfo = {
-  name: 'ocis-onlyoffice',
+  name: 'OnlyOffice',
   id: 'onlyoffice',
   icon: 'x-office-document',
   extensions: [
     {
       extension: 'docx',
-      handler: function(config, filePath, fileId) {
+      handler: function({ extensionConfig, filePath, fileId }) {
         window.open(
-          `${config.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
+          `${extensionConfig.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
           '_blank'
         )
       },
       newFileMenu: {
         menuTitle($gettext) {
-          return $gettext('New OnlyOFFICE document')
+          return $gettext('New OnlyOffice document')
         },
         icon: 'x-office-document'
       }
     },
     {
       extension: 'xlsx',
-      handler: function(config, filePath, fileId) {
+      handler: function({ extensionConfig, filePath, fileId }) {
         window.open(
-          `${config.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
+          `${extensionConfig.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
           '_blank'
         )
       }
     },
     {
       extension: 'pptx',
-      handler: function(config, filePath, fileId) {
+      handler: function({ extensionConfig, filePath, fileId }) {
         window.open(
-          `${config.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
+          `${extensionConfig.server}/apps/onlyoffice/${fileId}?filePath=${encodeURIComponent(filePath)}`,
           '_blank'
         )
       }
