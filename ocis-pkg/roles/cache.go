fca41af47f1278b9977a29e8aebbad18c5b1f201
Florian Schade <f.schade@icloud.com>
2021-01-20 18:32:33 +0100

    update roles cache to use sync.cache add tests for roles cache add changelog use strings in sync cache tests

--- ocis-pkg/roles/cache.go
+++ ocis-pkg/roles/cache.go
@@ -1,71 +1,35 @@
 package roles
 
 import (
-	"sync"
 	"time"
-
+	"github.com/owncloud/ocis/ocis-pkg/sync"
 	settings "github.com/owncloud/ocis/settings/pkg/proto/v0"
 )
 
-// entry extends a bundle and adds a TTL
-type entry struct {
-	*settings.Bundle
-	inserted time.Time
-}
-
 // cache is a cache implementation for roles, keyed by roleIDs.
 type cache struct {
-	entries map[string]entry
-	size    int
-	ttl     time.Duration
-	m       sync.Mutex
+	sc   sync.Cache
+	ttl  time.Duration
 }
 
 // newCache returns a new instance of Cache.
-func newCache(size int, ttl time.Duration) cache {
+func newCache(capacity int, ttl time.Duration) cache {
 	return cache{
-		size:    size,
 		ttl:     ttl,
-		entries: map[string]entry{},
+		sc: sync.NewCache(capacity),
 	}
 }
 
 // get gets a role-bundle by a given `roleID`.
 func (c *cache) get(roleID string) *settings.Bundle {
-	c.m.Lock()
-	defer c.m.Unlock()
-
-	if _, ok := c.entries[roleID]; ok {
-		return c.entries[roleID].Bundle
+	if ce := c.sc.Load(roleID); ce != nil {
+		return ce.V.(*settings.Bundle)
 	}
+
 	return nil
 }
 
 // set sets a roleID / role-bundle.
 func (c *cache) set(roleID string, value *settings.Bundle) {
-	c.m.Lock()
-	defer c.m.Unlock()
-
-	if !c.fits() {
-		c.evict()
-	}
-
-	c.entries[roleID] = entry{
-		value,
-		time.Now(),
-	}
-}
-
-// evict frees memory from the cache by removing entries that exceeded the cache TTL.
-func (c *cache) evict() {
-	for i := range c.entries {
-		if c.entries[i].inserted.Add(c.ttl).Before(time.Now()) {
-			delete(c.entries, i)
-		}
-	}
-}
-
-// fits returns whether the cache fits more entries.
-func (c *cache) fits() bool {
-	return c.size > len(c.entries)
+	c.sc.Store(roleID, value, time.Now().Add(c.ttl))
 }
