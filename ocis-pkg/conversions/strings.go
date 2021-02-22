1088faf95d311c0eed78895790931bc6494ee398
David Christofas <dchristofas@owncloud.com>
2021-02-22 18:54:46 +0100

    pre allocate slices
    Pre allocating slices with a target size reduces the number of allocations because we already know how big the slice will be and therefore need to allocate only once

--- ocis-pkg/conversions/strings.go
+++ ocis-pkg/conversions/strings.go
@@ -7,8 +7,8 @@ import (
 
 // StringToSliceString splits a string into a slice string according to separator
 func StringToSliceString(src string, sep string) []string {
-	var parts []string
 	parsed := strings.Split(src, sep)
+	parts := make([]string, 0, len(parsed))
 	for _, v := range parsed {
 		parts = append(parts, strings.TrimSpace(v))
 	}
