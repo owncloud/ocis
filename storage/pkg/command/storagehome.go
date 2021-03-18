0e3b97ac03677e56f76b7e75c28af7330018e5b1
A.Unger <zyxancf@gmail.com>
2021-03-18 11:45:23 +0100

    refactor storage-home tracing config

--- storage/pkg/command/storagehome.go
+++ storage/pkg/command/storagehome.go
@@ -16,6 +16,7 @@ import (
 	"github.com/owncloud/ocis/storage/pkg/config"
 	"github.com/owncloud/ocis/storage/pkg/flagset"
 	"github.com/owncloud/ocis/storage/pkg/server/debug"
+	"github.com/owncloud/ocis/storage/pkg/tracing"
 	"github.com/thejerf/suture/v4"
 )
 
@@ -33,133 +34,56 @@ func StorageHome(cfg *config.Config) *cli.Command {
 		Action: func(c *cli.Context) error {
 			logger := NewLogger(cfg)
 
-			if cfg.Tracing.Enabled {
-				switch t := cfg.Tracing.Type; t {
-				case "agent":
-					logger.Error().
-						Str("type", t).
-						Msg("Storage only supports the jaeger tracing backend")
+			tracing.Configure(cfg, logger)
 
-				case "jaeger":
-					logger.Info().
-						Str("type", t).
-						Msg("configuring storage to use the jaeger tracing backend")
-
-				case "zipkin":
-					logger.Error().
-						Str("type", t).
-						Msg("Storage only supports the jaeger tracing backend")
-
-				default:
-					logger.Warn().
-						Str("type", t).
-						Msg("Unknown tracing backend")
-				}
-
-			} else {
-				logger.Debug().
-					Msg("Tracing is not enabled")
-			}
-
-			var (
-				gr          = run.Group{}
-				ctx, cancel = context.WithCancel(context.Background())
-				//metrics     = metrics.New()
-			)
-
+			gr := run.Group{}
+			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
-			{
-				uuid := uuid.Must(uuid.NewV4())
-				pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
+			uuid := uuid.Must(uuid.NewV4())
+			pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
 
-				// override driver enable home option with home config
-				if cfg.Reva.Storages.Home.EnableHome {
-					cfg.Reva.Storages.Common.EnableHome = true
-					cfg.Reva.Storages.EOS.EnableHome = true
-					cfg.Reva.Storages.Local.EnableHome = true
-					cfg.Reva.Storages.OwnCloud.EnableHome = true
-					cfg.Reva.Storages.S3.EnableHome = true
-				}
-				rcfg := map[string]interface{}{
-					"core": map[string]interface{}{
-						"max_cpus":             cfg.Reva.StorageHome.MaxCPUs,
-						"tracing_enabled":      cfg.Tracing.Enabled,
-						"tracing_endpoint":     cfg.Tracing.Endpoint,
-						"tracing_collector":    cfg.Tracing.Collector,
-						"tracing_service_name": c.Command.Name,
-					},
-					"shared": map[string]interface{}{
-						"jwt_secret": cfg.Reva.JWTSecret,
-						"gatewaysvc": cfg.Reva.Gateway.Endpoint,
-					},
-					"grpc": map[string]interface{}{
-						"network": cfg.Reva.StorageHome.GRPCNetwork,
-						"address": cfg.Reva.StorageHome.GRPCAddr,
-						// TODO build services dynamically
-						"services": map[string]interface{}{
-							"storageprovider": map[string]interface{}{
-								"driver":             cfg.Reva.StorageHome.Driver,
-								"drivers":            drivers(cfg),
-								"mount_path":         cfg.Reva.StorageHome.MountPath,
-								"mount_id":           cfg.Reva.StorageHome.MountID,
-								"expose_data_server": cfg.Reva.StorageHome.ExposeDataServer,
-								"data_server_url":    cfg.Reva.StorageHome.DataServerURL,
-								"tmp_folder":         cfg.Reva.StorageHome.TempFolder,
-							},
-						},
-					},
-					"http": map[string]interface{}{
-						"network": cfg.Reva.StorageHome.HTTPNetwork,
-						"address": cfg.Reva.StorageHome.HTTPAddr,
-						// TODO build services dynamically
-						"services": map[string]interface{}{
-							"dataprovider": map[string]interface{}{
-								"prefix":      cfg.Reva.StorageHome.HTTPPrefix,
-								"driver":      cfg.Reva.StorageHome.Driver,
-								"drivers":     drivers(cfg),
-								"timeout":     86400,
-								"insecure":    true,
-								"disable_tus": false,
-							},
-						},
-					},
-				}
-
-				gr.Add(func() error {
-					runtime.RunWithOptions(
-						rcfg,
-						pidFile,
-						runtime.WithLogger(&logger.Logger),
-					)
-					return nil
-				}, func(_ error) {
-					logger.Info().
-						Str("server", c.Command.Name).
-						Msg("Shutting down server")
-
-					cancel()
-				})
+			// override driver enable home option with home config
+			if cfg.Reva.Storages.Home.EnableHome {
+				cfg.Reva.Storages.Common.EnableHome = true
+				cfg.Reva.Storages.EOS.EnableHome = true
+				cfg.Reva.Storages.Local.EnableHome = true
+				cfg.Reva.Storages.OwnCloud.EnableHome = true
+				cfg.Reva.Storages.S3.EnableHome = true
 			}
+			rcfg := storageHomeConfigFromStruct(c, cfg)
 
-			{
-				server, err := debug.Server(
-					debug.Name(c.Command.Name+"-debug"),
-					debug.Addr(cfg.Reva.StorageHome.DebugAddr),
-					debug.Logger(logger),
-					debug.Context(ctx),
-					debug.Config(cfg),
+			gr.Add(func() error {
+				runtime.RunWithOptions(
+					rcfg,
+					pidFile,
+					runtime.WithLogger(&logger.Logger),
 				)
+				return nil
+			}, func(_ error) {
+				logger.Info().
+					Str("server", c.Command.Name).
+					Msg("Shutting down server")
 
-				if err != nil {
-					logger.Info().Err(err).Str("server", c.Command.Name+"-debug").Msg("Failed to initialize server")
-					return err
-				}
+				cancel()
+			})
 
-				gr.Add(server.ListenAndServe, func(_ error) {
-					cancel()
-				})
+			debugServer, err := debug.Server(
+				debug.Name(c.Command.Name+"-debug"),
+				debug.Addr(cfg.Reva.StorageHome.DebugAddr),
+				debug.Logger(logger),
+				debug.Context(ctx),
+				debug.Config(cfg),
+			)
+
+			if err != nil {
+				logger.Info().Err(err).Str("server", c.Command.Name+"-debug").Msg("Failed to initialize server")
+				return err
 			}
+
+			gr.Add(debugServer.ListenAndServe, func(_ error) {
+				cancel()
+			})
 
 			if !cfg.Reva.StorageMetadata.Supervised {
 				sync.Trap(&gr, cancel)
@@ -168,6 +92,55 @@ func StorageHome(cfg *config.Config) *cli.Command {
 			return gr.Run()
 		},
 	}
+}
+
+// storageHomeConfigFromStruct will adapt an oCIS config struct into a reva mapstructure to start a reva service.
+func storageHomeConfigFromStruct(c *cli.Context, cfg *config.Config) map[string]interface{} {
+	rcfg := map[string]interface{}{
+		"core": map[string]interface{}{
+			"max_cpus":             cfg.Reva.StorageHome.MaxCPUs,
+			"tracing_enabled":      cfg.Tracing.Enabled,
+			"tracing_endpoint":     cfg.Tracing.Endpoint,
+			"tracing_collector":    cfg.Tracing.Collector,
+			"tracing_service_name": c.Command.Name,
+		},
+		"shared": map[string]interface{}{
+			"jwt_secret": cfg.Reva.JWTSecret,
+			"gatewaysvc": cfg.Reva.Gateway.Endpoint,
+		},
+		"grpc": map[string]interface{}{
+			"network": cfg.Reva.StorageHome.GRPCNetwork,
+			"address": cfg.Reva.StorageHome.GRPCAddr,
+			// TODO build services dynamically
+			"services": map[string]interface{}{
+				"storageprovider": map[string]interface{}{
+					"driver":             cfg.Reva.StorageHome.Driver,
+					"drivers":            drivers(cfg),
+					"mount_path":         cfg.Reva.StorageHome.MountPath,
+					"mount_id":           cfg.Reva.StorageHome.MountID,
+					"expose_data_server": cfg.Reva.StorageHome.ExposeDataServer,
+					"data_server_url":    cfg.Reva.StorageHome.DataServerURL,
+					"tmp_folder":         cfg.Reva.StorageHome.TempFolder,
+				},
+			},
+		},
+		"http": map[string]interface{}{
+			"network": cfg.Reva.StorageHome.HTTPNetwork,
+			"address": cfg.Reva.StorageHome.HTTPAddr,
+			// TODO build services dynamically
+			"services": map[string]interface{}{
+				"dataprovider": map[string]interface{}{
+					"prefix":      cfg.Reva.StorageHome.HTTPPrefix,
+					"driver":      cfg.Reva.StorageHome.Driver,
+					"drivers":     drivers(cfg),
+					"timeout":     86400,
+					"insecure":    true,
+					"disable_tus": false,
+				},
+			},
+		},
+	}
+	return rcfg
 }
 
 // StorageHomeSutureService allows for the storage-home command to be embedded and supervised by a suture supervisor tree.
