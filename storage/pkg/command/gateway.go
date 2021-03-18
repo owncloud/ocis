44fe360ef587700d3fad3462f2d1b39b58cd7360
A.Unger <zyxancf@gmail.com>
2021-03-18 11:36:54 +0100

    refactor gateway tracing config

--- storage/pkg/command/gateway.go
+++ storage/pkg/command/gateway.go
@@ -7,6 +7,8 @@ import (
 	"path"
 	"strings"
 
+	"github.com/owncloud/ocis/storage/pkg/tracing"
+
 	"github.com/owncloud/ocis/ocis-pkg/sync"
 
 	"github.com/cs3org/reva/cmd/revad/runtime"
@@ -39,161 +41,57 @@ func Gateway(cfg *config.Config) *cli.Command {
 		},
 		Action: func(c *cli.Context) error {
 			logger := NewLogger(cfg)
-
-			if cfg.Tracing.Enabled {
-				switch t := cfg.Tracing.Type; t {
-				case "agent":
-					logger.Error().
-						Str("type", t).
-						Msg("Reva only supports the jaeger tracing backend")
-
-				case "jaeger":
-					logger.Info().
-						Str("type", t).
-						Msg("configuring storage to use the jaeger tracing backend")
-
-				case "zipkin":
-					logger.Error().
-						Str("type", t).
-						Msg("Reva only supports the jaeger tracing backend")
-
-				default:
-					logger.Warn().
-						Str("type", t).
-						Msg("Unknown tracing backend")
-				}
-
-			} else {
-				logger.Debug().
-					Msg("Tracing is not enabled")
-			}
-
-			var (
-				gr          = run.Group{}
-				ctx, cancel = context.WithCancel(context.Background())
-				//metrics     = metrics.New()
-			)
-
+			tracing.Configure(cfg, logger)
+			gr := run.Group{}
+			ctx, cancel := context.WithCancel(context.Background())
+			uuid := uuid.Must(uuid.NewV4())
+			pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
+			rcfg := gatewayConfigFromStruct(c, cfg)
 			defer cancel()
 
-			{
-
-				uuid := uuid.Must(uuid.NewV4())
-				pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
-
-				rcfg := map[string]interface{}{
-					"core": map[string]interface{}{
-						"max_cpus":             cfg.Reva.Users.MaxCPUs,
-						"tracing_enabled":      cfg.Tracing.Enabled,
-						"tracing_endpoint":     cfg.Tracing.Endpoint,
-						"tracing_collector":    cfg.Tracing.Collector,
-						"tracing_service_name": c.Command.Name,
-					},
-					"shared": map[string]interface{}{
-						"jwt_secret": cfg.Reva.JWTSecret,
-						"gatewaysvc": cfg.Reva.Gateway.Endpoint,
-					},
-					"grpc": map[string]interface{}{
-						"network": cfg.Reva.Gateway.GRPCNetwork,
-						"address": cfg.Reva.Gateway.GRPCAddr,
-						// TODO build services dynamically
-						"services": map[string]interface{}{
-							"gateway": map[string]interface{}{
-								// registries is located on the gateway
-								"authregistrysvc":    cfg.Reva.Gateway.Endpoint,
-								"storageregistrysvc": cfg.Reva.Gateway.Endpoint,
-								"appregistrysvc":     cfg.Reva.Gateway.Endpoint,
-								// user metadata is located on the users services
-								"preferencessvc":   cfg.Reva.Users.Endpoint,
-								"userprovidersvc":  cfg.Reva.Users.Endpoint,
-								"groupprovidersvc": cfg.Reva.Groups.Endpoint,
-								// sharing is located on the sharing service
-								"usershareprovidersvc":          cfg.Reva.Sharing.Endpoint,
-								"publicshareprovidersvc":        cfg.Reva.Sharing.Endpoint,
-								"ocmshareprovidersvc":           cfg.Reva.Sharing.Endpoint,
-								"commit_share_to_storage_grant": cfg.Reva.Gateway.CommitShareToStorageGrant,
-								"commit_share_to_storage_ref":   cfg.Reva.Gateway.CommitShareToStorageRef,
-								"share_folder":                  cfg.Reva.Gateway.ShareFolder, // ShareFolder is the location where to create shares in the recipient's storage provider.
-								// other
-								"disable_home_creation_on_login": cfg.Reva.Gateway.DisableHomeCreationOnLogin,
-								"datagateway":                    cfg.Reva.DataGateway.PublicURL,
-								"transfer_shared_secret":         cfg.Reva.TransferSecret,
-								"transfer_expires":               cfg.Reva.TransferExpires,
-								"home_mapping":                   cfg.Reva.Gateway.HomeMapping,
-								"etag_cache_ttl":                 cfg.Reva.Gateway.EtagCacheTTL,
-							},
-							"authregistry": map[string]interface{}{
-								"driver": "static",
-								"drivers": map[string]interface{}{
-									"static": map[string]interface{}{
-										"rules": map[string]interface{}{
-											"basic":        cfg.Reva.AuthBasic.Endpoint,
-											"bearer":       cfg.Reva.AuthBearer.Endpoint,
-											"publicshares": cfg.Reva.StoragePublicLink.Endpoint,
-										},
-									},
-								},
-							},
-							"storageregistry": map[string]interface{}{
-								"driver": cfg.Reva.StorageRegistry.Driver,
-								"drivers": map[string]interface{}{
-									"static": map[string]interface{}{
-										"home_provider": cfg.Reva.StorageRegistry.HomeProvider,
-										"rules":         rules(cfg),
-									},
-								},
-							},
-						},
-					},
-				}
-
-				gr.Add(func() error {
-					err := external.RegisterGRPCEndpoint(
-						ctx,
-						"com.owncloud.storage",
-						uuid.String(),
-						cfg.Reva.Gateway.GRPCAddr,
-						logger,
-					)
-
-					if err != nil {
-						return err
-					}
-
-					runtime.RunWithOptions(
-						rcfg,
-						pidFile,
-						runtime.WithLogger(&logger.Logger),
-					)
-					return nil
-				}, func(_ error) {
-					logger.Info().
-						Str("server", c.Command.Name).
-						Msg("Shutting down server")
-
-					cancel()
-				})
-
-			}
-
-			{
-				server, err := debug.Server(
-					debug.Name(c.Command.Name+"-debug"),
-					debug.Addr(cfg.Reva.Gateway.DebugAddr),
-					debug.Logger(logger),
-					debug.Context(ctx),
-					debug.Config(cfg),
+			gr.Add(func() error {
+				err := external.RegisterGRPCEndpoint(
+					ctx,
+					"com.owncloud.storage",
+					uuid.String(),
+					cfg.Reva.Gateway.GRPCAddr,
+					logger,
 				)
 
 				if err != nil {
-					logger.Info().Err(err).Str("server", "debug").Msg("Failed to initialize server")
 					return err
 				}
 
-				gr.Add(server.ListenAndServe, func(_ error) {
-					cancel()
-				})
+				runtime.RunWithOptions(
+					rcfg,
+					pidFile,
+					runtime.WithLogger(&logger.Logger),
+				)
+				return nil
+			}, func(_ error) {
+				logger.Info().
+					Str("server", c.Command.Name).
+					Msg("Shutting down server")
+
+				cancel()
+			})
+
+			debugServer, err := debug.Server(
+				debug.Name(c.Command.Name+"-debug"),
+				debug.Addr(cfg.Reva.Gateway.DebugAddr),
+				debug.Logger(logger),
+				debug.Context(ctx),
+				debug.Config(cfg),
+			)
+
+			if err != nil {
+				logger.Info().Err(err).Str("server", "debug").Msg("Failed to initialize server")
+				return err
 			}
+
+			gr.Add(debugServer.ListenAndServe, func(_ error) {
+				cancel()
+			})
 
 			if !cfg.Reva.StorageMetadata.Supervised {
 				sync.Trap(&gr, cancel)
@@ -202,6 +100,76 @@ func Gateway(cfg *config.Config) *cli.Command {
 			return gr.Run()
 		},
 	}
+}
+
+// gatewayConfigFromStruct will adapt an oCIS config struct into a reva mapstructure to start a reva service.
+func gatewayConfigFromStruct(c *cli.Context, cfg *config.Config) map[string]interface{} {
+	rcfg := map[string]interface{}{
+		"core": map[string]interface{}{
+			"max_cpus":             cfg.Reva.Users.MaxCPUs,
+			"tracing_enabled":      cfg.Tracing.Enabled,
+			"tracing_endpoint":     cfg.Tracing.Endpoint,
+			"tracing_collector":    cfg.Tracing.Collector,
+			"tracing_service_name": c.Command.Name,
+		},
+		"shared": map[string]interface{}{
+			"jwt_secret": cfg.Reva.JWTSecret,
+			"gatewaysvc": cfg.Reva.Gateway.Endpoint,
+		},
+		"grpc": map[string]interface{}{
+			"network": cfg.Reva.Gateway.GRPCNetwork,
+			"address": cfg.Reva.Gateway.GRPCAddr,
+			// TODO build services dynamically
+			"services": map[string]interface{}{
+				"gateway": map[string]interface{}{
+					// registries is located on the gateway
+					"authregistrysvc":    cfg.Reva.Gateway.Endpoint,
+					"storageregistrysvc": cfg.Reva.Gateway.Endpoint,
+					"appregistrysvc":     cfg.Reva.Gateway.Endpoint,
+					// user metadata is located on the users services
+					"preferencessvc":   cfg.Reva.Users.Endpoint,
+					"userprovidersvc":  cfg.Reva.Users.Endpoint,
+					"groupprovidersvc": cfg.Reva.Groups.Endpoint,
+					// sharing is located on the sharing service
+					"usershareprovidersvc":          cfg.Reva.Sharing.Endpoint,
+					"publicshareprovidersvc":        cfg.Reva.Sharing.Endpoint,
+					"ocmshareprovidersvc":           cfg.Reva.Sharing.Endpoint,
+					"commit_share_to_storage_grant": cfg.Reva.Gateway.CommitShareToStorageGrant,
+					"commit_share_to_storage_ref":   cfg.Reva.Gateway.CommitShareToStorageRef,
+					"share_folder":                  cfg.Reva.Gateway.ShareFolder, // ShareFolder is the location where to create shares in the recipient's storage provider.
+					// other
+					"disable_home_creation_on_login": cfg.Reva.Gateway.DisableHomeCreationOnLogin,
+					"datagateway":                    cfg.Reva.DataGateway.PublicURL,
+					"transfer_shared_secret":         cfg.Reva.TransferSecret,
+					"transfer_expires":               cfg.Reva.TransferExpires,
+					"home_mapping":                   cfg.Reva.Gateway.HomeMapping,
+					"etag_cache_ttl":                 cfg.Reva.Gateway.EtagCacheTTL,
+				},
+				"authregistry": map[string]interface{}{
+					"driver": "static",
+					"drivers": map[string]interface{}{
+						"static": map[string]interface{}{
+							"rules": map[string]interface{}{
+								"basic":        cfg.Reva.AuthBasic.Endpoint,
+								"bearer":       cfg.Reva.AuthBearer.Endpoint,
+								"publicshares": cfg.Reva.StoragePublicLink.Endpoint,
+							},
+						},
+					},
+				},
+				"storageregistry": map[string]interface{}{
+					"driver": cfg.Reva.StorageRegistry.Driver,
+					"drivers": map[string]interface{}{
+						"static": map[string]interface{}{
+							"home_provider": cfg.Reva.StorageRegistry.HomeProvider,
+							"rules":         rules(cfg),
+						},
+					},
+				},
+			},
+		},
+	}
+	return rcfg
 }
 
 func rules(cfg *config.Config) map[string]interface{} {
