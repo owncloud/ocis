f53d00b4ffc463448151034952d6262f1322a760
A.Unger <zyxancf@gmail.com>
2021-03-18 11:51:07 +0100

    refactor users tracing config

--- storage/pkg/command/users.go
+++ storage/pkg/command/users.go
@@ -16,6 +16,7 @@ import (
 	"github.com/owncloud/ocis/storage/pkg/config"
 	"github.com/owncloud/ocis/storage/pkg/flagset"
 	"github.com/owncloud/ocis/storage/pkg/server/debug"
+	"github.com/owncloud/ocis/storage/pkg/tracing"
 	"github.com/thejerf/suture/v4"
 )
 
@@ -33,39 +34,10 @@ func Users(cfg *config.Config) *cli.Command {
 		Action: func(c *cli.Context) error {
 			logger := NewLogger(cfg)
 
-			if cfg.Tracing.Enabled {
-				switch t := cfg.Tracing.Type; t {
-				case "agent":
-					logger.Error().
-						Str("type", t).
-						Msg("Reva only supports the jaeger tracing backend")
+			tracing.Configure(cfg, logger)
 
-				case "jaeger":
-					logger.Info().
-						Str("type", t).
-						Msg("configuring storage to use the jaeger tracing backend")
-
-				case "zipkin":
-					logger.Error().
-						Str("type", t).
-						Msg("Reva only supports the jaeger tracing backend")
-
-				default:
-					logger.Warn().
-						Str("type", t).
-						Msg("Unknown tracing backend")
-				}
-
-			} else {
-				logger.Debug().
-					Msg("Tracing is not enabled")
-			}
-
-			var (
-				gr          = run.Group{}
-				ctx, cancel = context.WithCancel(context.Background())
-				//metrics     = metrics.New()
-			)
+			gr := run.Group{}
+			ctx, cancel := context.WithCancel(context.Background())
 
 			defer cancel()
 
@@ -76,105 +48,42 @@ func Users(cfg *config.Config) *cli.Command {
 				}
 			}
 
-			{
-				uuid := uuid.Must(uuid.NewV4())
-				pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
+			uuid := uuid.Must(uuid.NewV4())
+			pidFile := path.Join(os.TempDir(), "revad-"+c.Command.Name+"-"+uuid.String()+".pid")
 
-				rcfg := map[string]interface{}{
-					"core": map[string]interface{}{
-						"max_cpus":             cfg.Reva.Users.MaxCPUs,
-						"tracing_enabled":      cfg.Tracing.Enabled,
-						"tracing_endpoint":     cfg.Tracing.Endpoint,
-						"tracing_collector":    cfg.Tracing.Collector,
-						"tracing_service_name": c.Command.Name,
-					},
-					"shared": map[string]interface{}{
-						"jwt_secret": cfg.Reva.JWTSecret,
-					},
-					"grpc": map[string]interface{}{
-						"network": cfg.Reva.Users.GRPCNetwork,
-						"address": cfg.Reva.Users.GRPCAddr,
-						// TODO build services dynamically
-						"services": map[string]interface{}{
-							"userprovider": map[string]interface{}{
-								"driver": cfg.Reva.Users.Driver,
-								"drivers": map[string]interface{}{
-									"json": map[string]interface{}{
-										"users": cfg.Reva.Users.JSON,
-									},
-									"ldap": map[string]interface{}{
-										"hostname":        cfg.Reva.LDAP.Hostname,
-										"port":            cfg.Reva.LDAP.Port,
-										"base_dn":         cfg.Reva.LDAP.BaseDN,
-										"userfilter":      cfg.Reva.LDAP.UserFilter,
-										"attributefilter": cfg.Reva.LDAP.UserAttributeFilter,
-										"findfilter":      cfg.Reva.LDAP.UserFindFilter,
-										"groupfilter":     cfg.Reva.LDAP.UserGroupFilter,
-										"bind_username":   cfg.Reva.LDAP.BindDN,
-										"bind_password":   cfg.Reva.LDAP.BindPassword,
-										"idp":             cfg.Reva.LDAP.IDP,
-										"schema": map[string]interface{}{
-											"dn":          "dn",
-											"uid":         cfg.Reva.LDAP.UserSchema.UID,
-											"mail":        cfg.Reva.LDAP.UserSchema.Mail,
-											"displayName": cfg.Reva.LDAP.UserSchema.DisplayName,
-											"cn":          cfg.Reva.LDAP.UserSchema.CN,
-											"uidNumber":   cfg.Reva.LDAP.UserSchema.UIDNumber,
-											"gidNumber":   cfg.Reva.LDAP.UserSchema.GIDNumber,
-										},
-									},
-									"rest": map[string]interface{}{
-										"client_id":                    cfg.Reva.UserGroupRest.ClientID,
-										"client_secret":                cfg.Reva.UserGroupRest.ClientSecret,
-										"redis_address":                cfg.Reva.UserGroupRest.RedisAddress,
-										"redis_username":               cfg.Reva.UserGroupRest.RedisUsername,
-										"redis_password":               cfg.Reva.UserGroupRest.RedisPassword,
-										"user_groups_cache_expiration": cfg.Reva.Users.UserGroupsCacheExpiration,
-										"id_provider":                  cfg.Reva.UserGroupRest.IDProvider,
-										"api_base_url":                 cfg.Reva.UserGroupRest.APIBaseURL,
-										"oidc_token_endpoint":          cfg.Reva.UserGroupRest.OIDCTokenEndpoint,
-										"target_api":                   cfg.Reva.UserGroupRest.TargetAPI,
-									},
-								},
-							},
-						},
-					},
-				}
+			rcfg := usersConfigFromStruct(c, cfg)
 
-				gr.Add(func() error {
-					runtime.RunWithOptions(
-						rcfg,
-						pidFile,
-						runtime.WithLogger(&logger.Logger),
-					)
-					return nil
-				}, func(_ error) {
-					logger.Info().
-						Str("server", c.Command.Name).
-						Msg("Shutting down server")
-
-					cancel()
-				})
-			}
-
-			{
-				server, err := debug.Server(
-					debug.Name(c.Command.Name+"-debug"),
-					debug.Addr(cfg.Reva.Users.DebugAddr),
-					debug.Logger(logger),
-					debug.Context(ctx),
-					debug.Config(cfg),
+			gr.Add(func() error {
+				runtime.RunWithOptions(
+					rcfg,
+					pidFile,
+					runtime.WithLogger(&logger.Logger),
 				)
+				return nil
+			}, func(_ error) {
+				logger.Info().
+					Str("server", c.Command.Name).
+					Msg("Shutting down server")
 
-				if err != nil {
-					logger.Info().Err(err).Str("server", c.Command.Name+"-debug").Msg("Failed to initialize server")
-					return err
-				}
+				cancel()
+			})
 
-				gr.Add(server.ListenAndServe, func(_ error) {
-					cancel()
-				})
+			debugServer, err := debug.Server(
+				debug.Name(c.Command.Name+"-debug"),
+				debug.Addr(cfg.Reva.Users.DebugAddr),
+				debug.Logger(logger),
+				debug.Context(ctx),
+				debug.Config(cfg),
+			)
+
+			if err != nil {
+				logger.Info().Err(err).Str("server", c.Command.Name+"-debug").Msg("Failed to initialize server")
+				return err
 			}
+
+			gr.Add(debugServer.ListenAndServe, func(_ error) {
+				cancel()
+			})
 
 			if !cfg.Reva.StorageMetadata.Supervised {
 				sync.Trap(&gr, cancel)
@@ -183,6 +92,71 @@ func Users(cfg *config.Config) *cli.Command {
 			return gr.Run()
 		},
 	}
+}
+
+// usersConfigFromStruct will adapt an oCIS config struct into a reva mapstructure to start a reva service.
+func usersConfigFromStruct(c *cli.Context, cfg *config.Config) map[string]interface{} {
+	rcfg := map[string]interface{}{
+		"core": map[string]interface{}{
+			"max_cpus":             cfg.Reva.Users.MaxCPUs,
+			"tracing_enabled":      cfg.Tracing.Enabled,
+			"tracing_endpoint":     cfg.Tracing.Endpoint,
+			"tracing_collector":    cfg.Tracing.Collector,
+			"tracing_service_name": c.Command.Name,
+		},
+		"shared": map[string]interface{}{
+			"jwt_secret": cfg.Reva.JWTSecret,
+		},
+		"grpc": map[string]interface{}{
+			"network": cfg.Reva.Users.GRPCNetwork,
+			"address": cfg.Reva.Users.GRPCAddr,
+			// TODO build services dynamically
+			"services": map[string]interface{}{
+				"userprovider": map[string]interface{}{
+					"driver": cfg.Reva.Users.Driver,
+					"drivers": map[string]interface{}{
+						"json": map[string]interface{}{
+							"users": cfg.Reva.Users.JSON,
+						},
+						"ldap": map[string]interface{}{
+							"hostname":        cfg.Reva.LDAP.Hostname,
+							"port":            cfg.Reva.LDAP.Port,
+							"base_dn":         cfg.Reva.LDAP.BaseDN,
+							"userfilter":      cfg.Reva.LDAP.UserFilter,
+							"attributefilter": cfg.Reva.LDAP.UserAttributeFilter,
+							"findfilter":      cfg.Reva.LDAP.UserFindFilter,
+							"groupfilter":     cfg.Reva.LDAP.UserGroupFilter,
+							"bind_username":   cfg.Reva.LDAP.BindDN,
+							"bind_password":   cfg.Reva.LDAP.BindPassword,
+							"idp":             cfg.Reva.LDAP.IDP,
+							"schema": map[string]interface{}{
+								"dn":          "dn",
+								"uid":         cfg.Reva.LDAP.UserSchema.UID,
+								"mail":        cfg.Reva.LDAP.UserSchema.Mail,
+								"displayName": cfg.Reva.LDAP.UserSchema.DisplayName,
+								"cn":          cfg.Reva.LDAP.UserSchema.CN,
+								"uidNumber":   cfg.Reva.LDAP.UserSchema.UIDNumber,
+								"gidNumber":   cfg.Reva.LDAP.UserSchema.GIDNumber,
+							},
+						},
+						"rest": map[string]interface{}{
+							"client_id":                    cfg.Reva.UserGroupRest.ClientID,
+							"client_secret":                cfg.Reva.UserGroupRest.ClientSecret,
+							"redis_address":                cfg.Reva.UserGroupRest.RedisAddress,
+							"redis_username":               cfg.Reva.UserGroupRest.RedisUsername,
+							"redis_password":               cfg.Reva.UserGroupRest.RedisPassword,
+							"user_groups_cache_expiration": cfg.Reva.Users.UserGroupsCacheExpiration,
+							"id_provider":                  cfg.Reva.UserGroupRest.IDProvider,
+							"api_base_url":                 cfg.Reva.UserGroupRest.APIBaseURL,
+							"oidc_token_endpoint":          cfg.Reva.UserGroupRest.OIDCTokenEndpoint,
+							"target_api":                   cfg.Reva.UserGroupRest.TargetAPI,
+						},
+					},
+				},
+			},
+		},
+	}
+	return rcfg
 }
 
 // UsersProviderService allows for the storage-userprovider command to be embedded and supervised by a suture supervisor tree.
