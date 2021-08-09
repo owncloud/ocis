ac087d0daee4f1605e3508238421c9f7d439eccb
A.Unger <zyxancf@gmail.com>
2021-08-09 17:28:00 +0200

    replace opencensus everywhere

--- graph/pkg/tracing/tracing.go
+++ graph/pkg/tracing/tracing.go
@@ -1,87 +1,58 @@
 package tracing
 
 import (
-	"time"
-
-	"contrib.go.opencensus.io/exporter/jaeger"
-	"contrib.go.opencensus.io/exporter/ocagent"
-	"contrib.go.opencensus.io/exporter/zipkin"
-	openzipkin "github.com/openzipkin/zipkin-go"
-	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
 	"github.com/owncloud/ocis/graph/pkg/config"
 	"github.com/owncloud/ocis/ocis-pkg/log"
-	"go.opencensus.io/stats/view"
-	"go.opencensus.io/trace"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+var (
+	// Propagator ensures the entire module uses the same trace propagation strategy.
+	Propagator propagation.TextMapPropagator
+
+	// TraceProvider is the global trace provider for the proxy service.
+	TraceProvider = sdktrace.NewTracerProvider()
 )
 
 func Configure(cfg *config.Config, logger log.Logger) error {
 	if cfg.Tracing.Enabled {
 		switch t := cfg.Tracing.Type; t {
-		case "agent":
-			exporter, err := ocagent.NewExporter(
-				ocagent.WithReconnectionPeriod(5*time.Second),
-				ocagent.WithAddress(cfg.Tracing.Endpoint),
-				ocagent.WithServiceName(cfg.Tracing.Service),
-			)
-			if err != nil {
-				logger.Error().
-					Err(err).
-					Str("endpoint", cfg.Tracing.Endpoint).
-					Str("collector", cfg.Tracing.Collector).
-					Msg("Failed to create agent tracing")
-				return err
-			}
-			trace.RegisterExporter(exporter)
-			view.RegisterExporter(exporter)
 		case "jaeger":
-			exporter, err := jaeger.NewExporter(
-				jaeger.Options{
-					AgentEndpoint:     cfg.Tracing.Endpoint,
-					CollectorEndpoint: cfg.Tracing.Collector,
-					Process: jaeger.Process{
-						ServiceName: cfg.Tracing.Service,
-					},
-				},
-			)
-			if err != nil {
-				logger.Error().
-					Err(err).
-					Str("endpoint", cfg.Tracing.Endpoint).
-					Str("collector", cfg.Tracing.Collector).
-					Msg("Failed to create jaeger tracing")
-				return err
+			{
+				exp, err := jaeger.New(
+					jaeger.WithCollectorEndpoint(
+						jaeger.WithEndpoint(cfg.Tracing.Collector),
+					),
+				)
+				if err != nil {
+					panic(err)
+				}
+
+				// set package level trace provider and propagator.
+				TraceProvider = sdktrace.NewTracerProvider(
+					sdktrace.WithBatcher(exp),
+					sdktrace.WithResource(resource.NewWithAttributes(
+						semconv.SchemaURL,
+						semconv.ServiceNameKey.String("graph")),
+					),
+				)
+
+				Propagator = propagation.NewCompositeTextMapPropagator(
+					propagation.Baggage{},
+					propagation.TraceContext{},
+				)
 			}
-			trace.RegisterExporter(exporter)
+		case "agent":
+			fallthrough
 		case "zipkin":
-			endpoint, err := openzipkin.NewEndpoint(
-				cfg.Tracing.Service,
-				cfg.Tracing.Endpoint,
-			)
-			if err != nil {
-				logger.Error().
-					Err(err).
-					Str("endpoint", cfg.Tracing.Endpoint).
-					Str("collector", cfg.Tracing.Collector).
-					Msg("Failed to create zipkin tracing")
-				return err
-			}
-			exporter := zipkin.NewExporter(
-				zipkinhttp.NewReporter(
-					cfg.Tracing.Collector,
-				),
-				endpoint,
-			)
-			trace.RegisterExporter(exporter)
+			fallthrough
 		default:
-			logger.Warn().
-				Str("type", t).
-				Msg("Unknown tracing backend")
+			logger.Warn().Str("type", t).Msg("Unknown tracing backend")
 		}
-		trace.ApplyConfig(
-			trace.Config{
-				DefaultSampler: trace.AlwaysSample(),
-			},
-		)
 	} else {
 		logger.Debug().
 			Msg("Tracing is not enabled")
