1d59da722532c272217da0c47dba575ed17f6875
Willy Kloucek <wkloucek@owncloud.com>
2021-10-08 07:38:05 +0200

    workaround automatic startup

--- deployments/examples/ocis_wopi/config/ocis-appdriver-onlyoffice/entrypoint-override.sh
+++ deployments/examples/ocis_wopi/config/ocis-appdriver-onlyoffice/entrypoint-override.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
-sleep 120 #TODO: app driver should try again until onlyoffice is up...
+apk add curl
 
-ocis storage-app-provider server
+#TODO: app driver itself should try again until OnlyOffice is up...
+
+retries=10
+while [[ $retries -gt 0 ]]; do
+    if curl --silent --show-error --fail http://onlyoffice/hosting/discovery > /dev/null; then
+        ocis storage-app-provider server
+    else
+        echo "OnlyOffice is not yet available, trying again in 10 seconds"
+        sleep 10
+        retries=$((retries - 1))
+    fi
+done
+echo 'OnlyOffice was not available after 100 seconds'
+exit 1
