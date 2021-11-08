ef1361846228e774c8132c57ac5e87dd965887dd
A.Unger <zyxancf@gmail.com>
2021-11-08 14:02:36 +0100

    new config framework in store

--- store/pkg/command/root.go
+++ store/pkg/command/root.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"os"
 
+	"github.com/owncloud/ocis/ocis-pkg/shared"
+
 	ociscfg "github.com/owncloud/ocis/ocis-pkg/config"
 	"github.com/owncloud/ocis/ocis-pkg/log"
 	"github.com/owncloud/ocis/ocis-pkg/version"
@@ -84,7 +86,9 @@ type SutureService struct {
 
 // NewSutureService creates a new store.SutureService
 func NewSutureService(cfg *ociscfg.Config) suture.Service {
-	cfg.Store.Log = cfg.Log
+	if (cfg.Accounts.Log == shared.Log{}) {
+		cfg.Accounts.Log = cfg.Log
+	}
 	return SutureService{
 		cfg: cfg.Store,
 	}
