e4742fbb53367af7b558d0386e34730cb780c1a6
Willy Kloucek <wkloucek@owncloud.com>
2021-11-09 15:40:22 +0100

    fix cs3 users example

--- deployments/examples/cs3_users_ocis/config/ocis/entrypoint-override.sh
+++ deployments/examples/cs3_users_ocis/config/ocis/entrypoint-override.sh
@@ -3,7 +3,6 @@
 set -e
 
 mkdir -p /var/tmp/ocis/.config/
-cp /config/proxy-config.json /var/tmp/ocis/.config/proxy-config.json
 cp /config/web-config.dist.json /var/tmp/ocis/.config/web-config.json
 sed -i 's/ocis.owncloud.test/'${OCIS_DOMAIN:-ocis.owncloud.test}'/g' /var/tmp/ocis/.config/web-config.json
 
@@ -14,9 +13,5 @@ sleep 10
 ocis kill accounts
 # stop builtin LDAP server since we use external LDAP only
 ocis kill glauth
-
-ocis kill proxy
-sleep 10
-ocis proxy server # workaround for loading proxy configuration
 
 wait # wait for oCIS to exit
