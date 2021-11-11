34840d62e3b2b31f5cf7ce7d68ebc8c33d6ccb01
A.Unger <zyxancf@gmail.com>
2021-11-11 14:06:40 +0100

    fix individual idp command

--- ocis/pkg/command/idp.go
+++ ocis/pkg/command/idp.go
@@ -3,15 +3,12 @@ package command
 import (
 	"github.com/owncloud/ocis/idp/pkg/command"
 	"github.com/owncloud/ocis/ocis-pkg/config"
-	"github.com/owncloud/ocis/ocis-pkg/shared"
 	"github.com/owncloud/ocis/ocis/pkg/register"
 	"github.com/urfave/cli/v2"
 )
 
 // IDPCommand is the entrypoint for the idp command.
 func IDPCommand(cfg *config.Config) *cli.Command {
-	var globalLog shared.Log
-
 	return &cli.Command{
 		Name:     "idp",
 		Usage:    "Start idp server",
@@ -24,16 +21,13 @@ func IDPCommand(cfg *config.Config) *cli.Command {
 				return err
 			}
 
-			globalLog = cfg.Log
+			if cfg.Commons != nil {
+				cfg.IDP.Commons = cfg.Commons
+			}
 
 			return nil
 		},
 		Action: func(c *cli.Context) error {
-			// if idp logging is empty in ocis.yaml
-			if (cfg.IDP.Log == shared.Log{}) && (globalLog != shared.Log{}) {
-				// we can safely inherit the global logging values.
-				cfg.IDP.Log = globalLog
-			}
 			idpCommand := command.Server(cfg.IDP)
 			if err := idpCommand.Before(c); err != nil {
 				return err
