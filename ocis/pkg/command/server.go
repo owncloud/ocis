fde5e3549644946a672321c430b16e49257d86d3
A.Unger <zyxancf@gmail.com>
2021-11-15 10:38:02 +0100

    fix commons propagation

--- ocis/pkg/command/server.go
+++ ocis/pkg/command/server.go
@@ -6,6 +6,8 @@ package command
 import (
 	"strings"
 
+	"github.com/owncloud/ocis/ocis-pkg/shared"
+
 	"github.com/owncloud/ocis/ocis-pkg/config"
 	"github.com/owncloud/ocis/ocis/pkg/register"
 	"github.com/owncloud/ocis/ocis/pkg/runtime"
@@ -26,6 +28,11 @@ func Server(cfg *config.Config) *cli.Command {
 			return ParseConfig(c, cfg)
 		},
 		Action: func(c *cli.Context) error {
+
+			cfg.Commons = &shared.Commons{
+				Log: &cfg.Log,
+			}
+
 			r := runtime.New(cfg)
 			return r.Start()
 		},
