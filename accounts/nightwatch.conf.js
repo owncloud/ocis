c7cdccf8da4ff245095f5a3642afc3277d4e6bee
pwengerter <pascal@wengerter.info>
2021-11-16 18:03:54 +0000

    Install web acceptance test dependencies for web, accounts, settins

--- accounts/nightwatch.conf.js
+++ accounts/nightwatch.conf.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const WEB_PATH = process.env.WEB_PATH
 
-const config = require(path.join(WEB_PATH, 'nightwatch.conf.js'))
+const config = require(path.join(WEB_PATH, 'tests/acceptance/nightwatch.conf.js'))
 
 config.page_objects_path = [WEB_PATH + '/tests/acceptance/pageObjects', 'ui/tests/acceptance/pageobjects']
 config.custom_commands_path = WEB_PATH + '/tests/acceptance/customCommands'
