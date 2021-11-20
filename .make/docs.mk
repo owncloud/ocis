9c3fb1cf4cd6aa8e5ab021304f20bd79ba6ae7d3
A.Unger <zyxancf@gmail.com>
2021-11-20 11:20:36 +0100

    comment out .make/docs.mk config-docs-generate target

--- .make/docs.mk
+++ .make/docs.mk
@@ -3,7 +3,7 @@ SKIP_CONFIG_DOCS_GENERATE ?= 0
 CONFIG_DOCS_BASE_PATH ?= ../docs/extensions
 
 #.PHONY: config-docs-generate
-#config-docs-generate: $(FLAEX)
+config-docs-generate: $(FLAEX)
 #	@if [ $(SKIP_CONFIG_DOCS_GENERATE) -ne 1 ]; then \
 #		$(FLAEX) >| $(CONFIG_DOCS_BASE_PATH)/$(NAME)/configuration.md \
 #	; fi;
