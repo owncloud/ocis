ca555761696298bc2f0d321150077b00ca31975c
Willy Kloucek <wkloucek@owncloud.com>
2021-11-23 11:19:51 +0100

    fix doc generation and reenable docs pipeline in CI

--- .make/docs.mk
+++ .make/docs.mk
@@ -2,8 +2,10 @@
 SKIP_CONFIG_DOCS_GENERATE ?= 0
 CONFIG_DOCS_BASE_PATH ?= ../docs/extensions
 
-#.PHONY: config-docs-generate
-#config-docs-generate: $(FLAEX)
+.PHONY: config-docs-generate
+config-docs-generate: #$(FLAEX)
+# since https://github.com/owncloud/ocis/pull/2708 flaex can no longer be used
+# TODO: how to document configuration
 #	@if [ $(SKIP_CONFIG_DOCS_GENERATE) -ne 1 ]; then \
 #		$(FLAEX) >| $(CONFIG_DOCS_BASE_PATH)/$(NAME)/configuration.md \
 #	; fi;
