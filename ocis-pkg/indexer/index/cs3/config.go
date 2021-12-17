41403d79521f0338e05b3774d792a2857965a9a5
Willy Kloucek <wkloucek@owncloud.com>
2021-12-17 17:59:26 +0100

    fix some TODOs

--- ocis-pkg/indexer/index/cs3/config.go
+++ ocis-pkg/indexer/index/cs3/config.go
@@ -4,7 +4,6 @@ import (
 	acccfg "github.com/owncloud/ocis/accounts/pkg/config"
 )
 
-//TODO: remove?
 // Config represents cs3conf. Should be deprecated in favor of config.Config.
 type Config struct {
 	ProviderAddr string
