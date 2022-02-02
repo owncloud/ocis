2417f29f7b80a7167230780c30bfd97121840191
Saw-jan <saw.jan.grg3e@gmail.com>
2022-02-02 17:29:31 +0545

    update docker-compose for running tests

--- deployments/examples/oc10_ocis_parallel/config/oc10/10-custom-config.sh
+++ deployments/examples/oc10_ocis_parallel/config/oc10/10-custom-config.sh
@@ -37,4 +37,9 @@ gomplate \
 occ market:upgrade --major web
 occ app:enable web
 
+# enable testing app
+echo "Cloning and enabling testing app..."
+git clone --depth 1 https://github.com/owncloud/testing.git /var/www/owncloud/apps/testing
+occ app:enable testing
+
 true
