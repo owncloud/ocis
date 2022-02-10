fa1c27167d02553ac2306cfd4c90957f951e3b91
jkoberg <jkoberg@owncloud.com>
2022-02-10 17:06:19 +0100

    remove unneeded files
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- nats/pkg/config/defaultconfig.go
+++ nats/pkg/config/defaultconfig.go
@@ -1,5 +1,8 @@
 package config
 
+// NOTE: Most of this configuration is not needed to keep it as simple as possible
+// TODO: Clean up unneeded configuration
+
 func DefaultConfig() *Config {
 	return &Config{
 		Service: Service{
--- nats/settings.go
+++
@@ -1,9 +0,0 @@
-package settings
-
-import (
-	"embed"
-)
-
-//go:generate make generate
-//go:embed assets/*
-var Assets embed.FS
