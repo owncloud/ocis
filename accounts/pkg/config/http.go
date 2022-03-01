08fd1aaee0fc3c701d0e72d15903514d1e3f5cfe
David Christofas <dchristofas@owncloud.com>
2022-03-01 16:49:26 +0100

    add config descriptions
    Co-Authored-By: Christian Richter <crichter@owncloud.com@>

--- accounts/pkg/config/http.go
+++ accounts/pkg/config/http.go
@@ -2,10 +2,10 @@ package config
 
 // HTTP defines the available http configuration.
 type HTTP struct {
-	Addr      string `env:"ACCOUNTS_HTTP_ADDR"`
+	Addr      string `env:"ACCOUNTS_HTTP_ADDR" desc:"The address of the http service."`
 	Namespace string
-	Root      string `env:"ACCOUNTS_HTTP_ROOT"`
-	CacheTTL  int    `env:"ACCOUNTS_CACHE_TTL"`
+	Root      string `env:"ACCOUNTS_HTTP_ROOT" desc:"The root path of the http service."`
+	CacheTTL  int    `env:"ACCOUNTS_CACHE_TTL" desc:"The cache time for the static assets."`
 	CORS      CORS
 }
 
