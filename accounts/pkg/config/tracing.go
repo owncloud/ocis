4a50d39a5ea466bc5bd62a6af1d76abbaf0b71c9
Willy Kloucek <wkloucek@owncloud.com>
2022-03-02 15:21:47 +0100

    revert ocisConfig changes part 3

--- accounts/pkg/config/tracing.go
+++ accounts/pkg/config/tracing.go
@@ -2,8 +2,8 @@ package config
 
 // Tracing defines the available tracing configuration.
 type Tracing struct {
-	Enabled   bool   `env:"OCIS_TRACING_ENABLED;ACCOUNTS_TRACING_ENABLED" desc:"Activates tracing."`
-	Type      string `env:"OCIS_TRACING_TYPE;ACCOUNTS_TRACING_TYPE"`
-	Endpoint  string `env:"OCIS_TRACING_ENDPOINT;ACCOUNTS_TRACING_ENDPOINT" desc:"The endpoint to the tracing collector."`
-	Collector string `env:"OCIS_TRACING_COLLECTOR;ACCOUNTS_TRACING_COLLECTOR" `
+	Enabled   bool   `ocisConfig:"enabled" env:"OCIS_TRACING_ENABLED;ACCOUNTS_TRACING_ENABLED" desc:"Activates tracing."`
+	Type      string `ocisConfig:"type" env:"OCIS_TRACING_TYPE;ACCOUNTS_TRACING_TYPE"`
+	Endpoint  string `ocisConfig:"endpoint" env:"OCIS_TRACING_ENDPOINT;ACCOUNTS_TRACING_ENDPOINT" desc:"The endpoint to the tracing collector."`
+	Collector string `ocisConfig:"collector" env:"OCIS_TRACING_COLLECTOR;ACCOUNTS_TRACING_COLLECTOR"`
 }
