77d9e77e91141b3a19222cd3e050d0330244ab86
Christian Richter <crichter@owncloud.com>
2022-03-09 17:03:20 +0100

    Update configextractor to support multiple templates

--- docs/helpers/configenvextractor.go
+++ docs/helpers/configenvextractor.go
@@ -11,9 +11,13 @@ import (
 	"text/template"
 )
 
+var targets = map[string]string{
+	"extractor.go.tmpl": "output/runner.go",
+}
+
 func main() {
 	fmt.Println("Getting relevant packages")
-	paths, err := filepath.Glob("../../*/pkg/config/defaultconfig.go")
+	paths, err := filepath.Glob("../../*/pkg/config/defaults/defaultconfig.go")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,27 +28,38 @@ func main() {
 	for i := range paths {
 		paths[i] = replacer.Replace(paths[i])
 	}
-	content, err := ioutil.ReadFile("extractor.go.tmpl")
+
+	for template, output := range targets {
+		GenerateIntermediateCode(template, output, paths)
+		RunIntermediateCode(output)
+	}
+	fmt.Println("Cleaning up")
+	os.Chdir("../")
+	os.RemoveAll("output")
+}
+
+func GenerateIntermediateCode(templatePath string, intermediateCodePath string, paths []string) {
+	content, err := ioutil.ReadFile(templatePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Generating intermediate go code")
+	fmt.Println("Generating intermediate go code for " + intermediateCodePath + " using template " + templatePath)
 	tpl := template.Must(template.New("").Parse(string(content)))
 	os.Mkdir("output", 0700)
-	runner, err := os.Create("output/runner.go")
+	runner, err := os.Create(intermediateCodePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	tpl.Execute(runner, paths)
-	fmt.Println("Running intermediate go code")
+}
+
+func RunIntermediateCode(intermediateCodePath string) {
+	fmt.Println("Running intermediate go code for " + intermediateCodePath)
 	os.Chdir("output")
 	os.Setenv("OCIS_BASE_DATA_PATH", "~/.ocis")
-	out, err := exec.Command("go", "run", "runner.go").Output()
+	out, err := exec.Command("go", "run", "../"+intermediateCodePath).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(out))
-	fmt.Println("Cleaning up")
-	os.Chdir("../")
-	os.RemoveAll("output")
 }
