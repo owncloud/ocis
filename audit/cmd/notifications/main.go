e5cae733b174d87fc31d9e8dfec1e7436252ab4f
Willy Kloucek <wkloucek@owncloud.com>
2022-03-14 14:21:37 +0100

    fix audit config docs

--- audit/cmd/notifications/main.go
+++ audit/cmd/notifications/main.go
@@ -4,11 +4,11 @@ import (
 	"os"
 
 	"github.com/owncloud/ocis/audit/pkg/command"
-	"github.com/owncloud/ocis/audit/pkg/config"
+	"github.com/owncloud/ocis/idm/pkg/config/defaults"
 )
 
 func main() {
-	if err := command.Execute(config.DefaultConfig()); err != nil {
+	if err := command.Execute(defaults.DefaultConfig()); err != nil {
 		os.Exit(1)
 	}
 }
