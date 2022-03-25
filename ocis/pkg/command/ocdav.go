d1d4794da221914530582c32f08b5da691cf102b
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-03-25 16:20:10 +0000

    add missing comands
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ ocis/pkg/command/ocdav.go
@@ -0,0 +1,28 @@
+package command
+
+import (
+	"github.com/owncloud/ocis/ocis-pkg/config"
+	"github.com/owncloud/ocis/ocis/pkg/register"
+	"github.com/owncloud/ocis/storage/pkg/command"
+	"github.com/urfave/cli/v2"
+)
+
+// OCDavCommand is the entrypoint for the ocdav command.
+func OCDavCommand(cfg *config.Config) *cli.Command {
+	return &cli.Command{
+		Name:     "ocdav",
+		Usage:    "start ocdav",
+		Category: "extensions",
+		Before: func(ctx *cli.Context) error {
+			return ParseStorageCommon(ctx, cfg)
+		},
+		Action: func(c *cli.Context) error {
+			origCmd := command.OCDav(cfg.Storage)
+			return handleOriginalAction(c, origCmd)
+		},
+	}
+}
+
+func init() {
+	register.AddCommand(OCDavCommand)
+}
