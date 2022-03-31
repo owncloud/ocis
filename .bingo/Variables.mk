dc9eed82281c861f569fa7d0077ca60c6e5713d8
Willy Kloucek <wkloucek@owncloud.com>
2022-03-31 20:00:24 +0200

    configure reva gateway endpoint for sharing driver

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -100,3 +100,4 @@ $(REFLEX): $(BINGO_DIR)/reflex.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/reflex-v0.3.1"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=reflex.mod -o=$(GOBIN)/reflex-v0.3.1 "github.com/cespare/reflex"
+
