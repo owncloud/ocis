bba190f9803abaa959e9754bc01217e41ef2f35d
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-04-14 08:19:51 +0000

    add ocis search command
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ ocis/pkg/command/search.go
@@ -0,0 +1,26 @@
+package command
+
+import (
+	"github.com/owncloud/ocis/ocis-pkg/config"
+	"github.com/owncloud/ocis/ocis-pkg/config/parser"
+	"github.com/owncloud/ocis/ocis/pkg/register"
+	"github.com/owncloud/ocis/search/pkg/command"
+	"github.com/urfave/cli/v2"
+)
+
+// SearchCommand is the entry point for the search command.
+func SearchCommand(cfg *config.Config) *cli.Command {
+	return &cli.Command{
+		Name:     cfg.Search.Service.Name,
+		Usage:    subcommandDescription(cfg.Search.Service.Name),
+		Category: "extensions",
+		Before: func(ctx *cli.Context) error {
+			return parser.ParseConfig(cfg)
+		},
+		Subcommands: command.GetCommands(cfg.Search),
+	}
+}
+
+func init() {
+	register.AddCommand(SearchCommand)
+}
