0644dc857a9e04d576ad9fef02d86cbc680314f1
Willy Kloucek <wkloucek@owncloud.com>
2022-04-19 14:23:50 +0200

    apply config flag to ocis server command

--- ocis/pkg/command/server.go
+++ ocis/pkg/command/server.go
@@ -3,6 +3,7 @@ package command
 import (
 	"github.com/owncloud/ocis/ocis-pkg/config"
 	"github.com/owncloud/ocis/ocis-pkg/config/parser"
+	"github.com/owncloud/ocis/ocis-pkg/log"
 	"github.com/owncloud/ocis/ocis-pkg/shared"
 	"github.com/owncloud/ocis/ocis/pkg/register"
 	"github.com/owncloud/ocis/ocis/pkg/runtime"
@@ -15,8 +16,23 @@ func Server(cfg *config.Config) *cli.Command {
 		Name:     "server",
 		Usage:    "start a fullstack server (runtime and all extensions in supervised mode)",
 		Category: "fullstack",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config-file",
+				Value:       cfg.ConfigFile,
+				Usage:       "config file to be loaded by the extension",
+				Destination: &cfg.ConfigFile,
+			},
+		},
 		Before: func(c *cli.Context) error {
-			return parser.ParseConfig(cfg)
+			err := parser.ParseConfig(cfg)
+			if err != nil {
+				logger := log.NewLogger(
+					log.Name("oCIS"),
+				)
+				logger.Error().Err(err).Msg("couldn't find the specified config file")
+			}
+			return err
 		},
 		Action: func(c *cli.Context) error {
 
