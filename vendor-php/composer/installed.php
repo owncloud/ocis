b802c787626a3cf13fa17f7f572beff68fab3817
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-04-21 15:25:27 +0545

    Remove jarnaiz/behat-junit-formatter from acceptance tests
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

---
+++ vendor-php/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'addbbdadeb17ff9af33d14dba62ce4ca045bcf2c',
+        'name' => 'owncloud/ocis',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bamarni/composer-bin-plugin' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../bamarni/composer-bin-plugin',
+            'aliases' => array(),
+            'reference' => '49934ffea764864788334c1485fbb08a4b852031',
+            'dev_requirement' => true,
+        ),
+        'owncloud/ocis' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'addbbdadeb17ff9af33d14dba62ce4ca045bcf2c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
