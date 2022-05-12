7457117dbf1e0cdb34d0919361c2d0c4994c4c05
Ralf Haferkamp <rhaferkamp@owncloud.com>
2022-05-12 12:58:55 +0200

    Remove unused config vars
    GroupMembersCacheExpiration is referenced nowhere. Currently we don't
    have any caching. LoginAttributes is not used by the reva groupprovider.

--- extensions/groups/pkg/config/config.go
+++ extensions/groups/pkg/config/config.go
@@ -20,9 +20,8 @@ type Config struct {
 
 	SkipUserGroupsInToken bool `yaml:"skip_user_groups_in_token" env:"GROUPS_SKIP_USER_GROUPS_IN_TOKEN"`
 
-	GroupMembersCacheExpiration int     `yaml:"group_members_cache_expiration"`
-	Driver                      string  `yaml:"driver"`
-	Drivers                     Drivers `yaml:"drivers"`
+	Driver  string  `yaml:"driver"`
+	Drivers Drivers `yaml:"drivers"`
 
 	Supervised bool            `yaml:"-"`
 	Context    context.Context `yaml:"-"`
@@ -79,7 +78,6 @@ type LDAPDriver struct {
 	GroupFilter      string          `yaml:"group_filter" env:"LDAP_GROUPFILTER;GROUPS_LDAP_USERFILTER"`
 	UserObjectClass  string          `yaml:"user_object_class" env:"LDAP_USER_OBJECTCLASS;GROUPS_LDAP_USER_OBJECTCLASS"`
 	GroupObjectClass string          `yaml:"group_object_class" env:"LDAP_GROUP_OBJECTCLASS;GROUPS_LDAP_GROUP_OBJECTCLASS"`
-	LoginAttributes  []string        `yaml:"login_attributes" env:"LDAP_LOGIN_ATTRIBUTES;GROUPS_LDAP_LOGIN_ATTRIBUTES"`
 	IDP              string          `yaml:"idp" env:"OCIS_URL;OCIS_OIDC_ISSUER;GROUPS_IDP_URL"`
 	UserSchema       LDAPUserSchema  `yaml:"user_schema"`
 	GroupSchema      LDAPGroupSchema `yaml:"group_schema"`
