4b73b32bc8521aadb1e199ac7c76fc86ab903b70
Willy Kloucek <wkloucek@owncloud.com>
2022-05-16 08:15:52 +0200

    remove unused transfer secret from app provider

--- extensions/app-registry/pkg/config/parser/parse.go
+++ extensions/app-registry/pkg/config/parser/parse.go
@@ -38,9 +38,5 @@ func Validate(cfg *config.Config) error {
 		return shared.MissingJWTTokenError(cfg.Service.Name)
 	}
 
-	if cfg.TransferSecret == "" {
-		return shared.MissingRevaTransferSecretError(cfg.Service.Name)
-	}
-
 	return nil
 }
