7d879274597d25a7b52baf1984407db25aaac1d4
Willy Kloucek <wkloucek@owncloud.com>
2022-05-18 09:47:02 +0200

    add descriptions to the debug config

--- extensions/gateway/pkg/config/config.go
+++ extensions/gateway/pkg/config/config.go
@@ -68,10 +68,10 @@ type Service struct {
 }
 
 type Debug struct {
-	Addr   string `yaml:"addr" env:"GATEWAY_DEBUG_ADDR"`
-	Token  string `yaml:"token" env:"GATEWAY_DEBUG_TOKEN"`
-	Pprof  bool   `yaml:"pprof" env:"GATEWAY_DEBUG_PPROF"`
-	Zpages bool   `yaml:"zpages" env:"GATEWAY_DEBUG_ZPAGES"`
+	Addr   string `yaml:"addr" env:"GATEWAY_DEBUG_ADDR" desc:"Bind address of the debug server, where metrics, health, config and debug endpoints will be exposed."`
+	Token  string `yaml:"token" env:"GATEWAY_DEBUG_TOKEN" desc:"Token to secure the metrics endpoint"`
+	Pprof  bool   `yaml:"pprof" env:"GATEWAY_DEBUG_PPROF" desc:"Enables pprof, which can be used for profiling"`
+	Zpages bool   `yaml:"zpages" env:"GATEWAY_DEBUG_ZPAGES" desc:"Enables zpages, which can  be used for collecting and viewing traces in-me"`
 }
 
 type GRPCConfig struct {
--- extensions/idm/pkg/config/debug.go
+++ extensions/idm/pkg/config/debug.go
@@ -2,8 +2,8 @@ package config
 
 // Debug defines the available debug configuration.
 type Debug struct {
-	Addr   string `yaml:"addr" env:"IDM_DEBUG_ADDR"`
-	Token  string `yaml:"token" env:"IDM_DEBUG_TOKEN"`
-	Pprof  bool   `yaml:"pprof" env:"IDM_DEBUG_PPROF"`
-	Zpages bool   `yaml:"zpages" env:"IDM_DEBUG_ZPAGES"`
+	Addr   string `yaml:"addr" env:"IDM_DEBUG_ADDR" desc:"Bind address of the debug server, where metrics, health, config and debug endpoints will be exposed."`
+	Token  string `yaml:"token" env:"IDM_DEBUG_TOKEN" desc:"Token to secure the metrics endpoint"`
+	Pprof  bool   `yaml:"pprof" env:"IDM_DEBUG_PPROF" desc:"Enables pprof, which can be used for profiling"`
+	Zpages bool   `yaml:"zpages" env:"IDM_DEBUG_ZPAGES" desc:"Enables zpages, which can  be used for collecting and viewing traces in-me"`
 }
