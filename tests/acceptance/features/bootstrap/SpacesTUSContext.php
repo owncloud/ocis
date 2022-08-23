59c4bd803fb46d886c8641bb3e061862fea9a657
Prarup Gurung <grgprarup@gmail.com>
2022-08-23 09:26:30 +0545

    Added TUSUpload related tests for /Shares in ocis

--- tests/acceptance/features/bootstrap/SpacesTUSContext.php
+++ tests/acceptance/features/bootstrap/SpacesTUSContext.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * ownCloud
  *
@@ -165,5 +166,145 @@ class SpacesTUSContext implements Context {
 	): void {
 		$this->spacesContext->setSpaceIDByName($user, $spaceName);
 		$this->tusContext->userUploadsAFileWithContentToUsingTus($user, $content, $resource);
+	}
+
+	/**
+	 * @When /^user "([^"]*)" uploads a file "([^"]*)" to "([^"]*)" with mtime "([^"]*)" via TUS inside of the space "([^"]*)" using the WebDAV API$/
+	 *
+	 * @param string $user
+	 * @param string $source
+	 * @param string $destination
+	 * @param string $mtime Time in human readable format is taken as input which is converted into milliseconds that is used by API
+	 * @param string $spaceName
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+	 * @throws GuzzleException
+	 */
+	public function userUploadsAFileToWithMtimeViaTusInsideOfTheSpaceUsingTheWebdavApi(
+		string $user,
+		string $source,
+		string $destination,
+		string $mtime,
+		string $spaceName
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->tusContext->userUploadsFileWithContentToWithMtimeUsingTUS($user, $source, $destination, $mtime);
+	}
+
+	/**
+	 * @Given /^user "([^"]*)" has uploaded file with checksum "([^"]*)" to the last created TUS Location with offset "([^"]*)" and content "([^"]*)" via TUS inside of the space "([^"]*)" using the WebDAV API$/
+	 *
+	 * @param string $user
+	 * @param string $checksum
+	 * @param string $offset
+	 * @param string $content
+	 * @param string $spaceName
+	 *
+	 * @return void
+	 * @throws Exception|GuzzleException
+	 */
+	public function userHasUploadedFileWithChecksumToTheLastCreatedTusLocationWithOffsetAndContentViaTusInsideOfTheSpaceUsingTheWebdavApi(
+		string $user,
+		string $checksum,
+		string $offset,
+		string $content,
+		string $spaceName
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->tusContext->userHasUploadedFileWithChecksum($user, $checksum, $offset, $content);
+	}
+
+	/**
+	 * @Given /^user "([^"]*)" uploads file with checksum "([^"]*)" to the last created TUS Location with offset "([^"]*)" and content "([^"]*)" via TUS inside of the space "([^"]*)" using the WebDAV API$/
+	 *
+	 * @param string $user
+	 * @param string $checksum
+	 * @param string $offset
+	 * @param string $content
+	 * @param string $spaceName
+	 *
+	 * @return void
+	 * @throws Exception|GuzzleException
+	 */
+	public function userUploadsFileWithChecksumToTheLastCreatedTusLocationWithOffsetAndContentViaTusInsideOfTheSpaceUsingTheWebdavApi(
+		string $user,
+		string $checksum,
+		string $offset,
+		string $content,
+		string $spaceName
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->tusContext->userUploadsFileWithChecksum($user, $checksum, $offset, $content);
+	}
+
+	/**
+	 * @When /^user "([^"]*)" sends a chunk to the last created TUS Location with offset "([^"]*)" and data "([^"]*)" with checksum "([^"]*)" via TUS inside of the space "([^"]*)" using the WebDAV API$/
+	 *
+	 * @param string $user
+	 * @param string $offset
+	 * @param string $data
+	 * @param string $checksum
+	 * @param string $spaceName
+	 *
+	 * @return void
+	 * @throws Exception|GuzzleException
+	 */
+	public function userSendsAChunkToTheLastCreatedTusLocationWithOffsetAndDataWithChecksumViaTusInsideOfTheSpaceUsingTheWebdavApi(
+		string $user,
+		string $offset,
+		string $data,
+		string $checksum,
+		string $spaceName
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->tusContext->userUploadsChunkFileWithChecksum($user, $offset, $data, $checksum);
+	}
+
+	/**
+	 * @When /^user "([^"]*)" overwrites recently shared file with offset "([^"]*)" and data "([^"]*)" with checksum "([^"]*)" via TUS inside of the space "([^"]*)" using the WebDAV API with these headers:$/
+	 *
+	 * @param string $user
+	 * @param string $offset
+	 * @param string $data
+	 * @param string $checksum
+	 * @param string $spaceName
+	 * @param TableNode $headers
+	 *
+	 * @return void
+	 * @throws GuzzleException
+	 */
+	public function userOverwritesRecentlySharedFileWithOffsetAndDataWithChecksumViaTusInsideOfTheSpaceUsingTheWebdavApiWithTheseHeaders(
+		string $user,
+		string $offset,
+		string $data,
+		string $checksum,
+		string $spaceName,
+		TableNode $headers
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->tusContext->userOverwritesFileWithChecksum($user, $offset, $data, $checksum, $headers);
+	}
+
+	/**
+	 * @Then /^as "([^"]*)" the mtime of the file "([^"]*)" in space "([^"]*)" should be "([^"]*)"$/
+	 *
+	 * @param string $user
+	 * @param string $resource
+	 * @param string $spaceName
+	 * @param string $mtime
+	 *
+	 * @return void
+	 * @throws Exception|GuzzleException
+	 */
+	public function theMtimeOfTheFileInSpaceShouldBe(
+		string $user,
+		string $resource,
+		string $spaceName,
+		string $mtime
+	): void {
+		$this->spacesContext->setSpaceIDByName($user, $spaceName);
+		$this->featureContext->theMtimeOfTheFileShouldBe($user, $resource, $mtime);
 	}
 }
