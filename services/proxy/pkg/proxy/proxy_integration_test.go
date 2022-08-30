62500fad10c2a5ddb125b224ddcd60d3097628d7
David Christofas <dchristofas@owncloud.com>
2022-08-30 12:30:12 +0200

    add missing unprotected flag and fix proxy test

--- services/proxy/pkg/proxy/proxy_integration_test.go
+++ services/proxy/pkg/proxy/proxy_integration_test.go
@@ -2,15 +2,17 @@ package proxy
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
+	"github.com/owncloud/ocis/v2/ocis-pkg/log"
 	"github.com/owncloud/ocis/v2/services/proxy/pkg/config"
+	"github.com/owncloud/ocis/v2/services/proxy/pkg/router"
 )
 
 func TestProxyIntegration(t *testing.T) {
@@ -114,6 +116,8 @@ func TestProxyIntegration(t *testing.T) {
 		t.Run(tests[k].id, func(t *testing.T) {
 			t.Parallel()
 			tc := tests[k]
+
+			rt := router.Middleware(nil, tc.conf, log.NewLogger())
 			rp := newTestProxy(testConfig(tc.conf), func(req *http.Request) *http.Response {
 				if got, want := req.URL.String(), tc.expect.String(); got != want {
 					t.Errorf("Proxied url should be %v got %v", want, got)
@@ -135,7 +139,7 @@ func TestProxyIntegration(t *testing.T) {
 			})
 
 			rr := httptest.NewRecorder()
-			rp.ServeHTTP(rr, tc.input)
+			rt(rp).ServeHTTP(rr, tc.input)
 			rsp := rr.Result()
 
 			if rsp.StatusCode != 200 {
@@ -204,7 +208,7 @@ func (tc *testCase) withRequest(method string, target string, body io.Reader) *t
 func (tc *testCase) expectProxyTo(strURL string) testCase {
 	pu, err := url.Parse(strURL)
 	if err != nil {
-		log.Fatalf("Error parsing %v", strURL)
+		panic(fmt.Sprintf("Error parsing %v", strURL))
 	}
 
 	tc.expect = pu
