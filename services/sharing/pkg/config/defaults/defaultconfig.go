40b7aaa7ec628320c870aab32810f0386f1f2aa1
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-09-09 11:54:18 +0000

    make jsoncs3 default public share backend
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/sharing/pkg/config/defaults/defaultconfig.go
+++ services/sharing/pkg/config/defaults/defaultconfig.go
@@ -53,7 +53,7 @@ func DefaultConfig() *config.Config {
 				DBName:     "owncloud",
 			},
 		},
-		PublicSharingDriver: "cs3",
+		PublicSharingDriver: "jsoncs3",
 		PublicSharingDrivers: config.PublicSharingDrivers{
 			JSON: config.PublicSharingJSONDriver{
 				File: filepath.Join(defaults.BaseDataPath(), "storage", "publicshares.json"),
@@ -137,6 +137,14 @@ func EnsureDefaults(cfg *config.Config) {
 
 	if cfg.PublicSharingDrivers.CS3.SystemUserID == "" && cfg.Commons != nil && cfg.Commons.SystemUserID != "" {
 		cfg.PublicSharingDrivers.CS3.SystemUserID = cfg.Commons.SystemUserID
+	}
+
+	if cfg.PublicSharingDrivers.JSONCS3.SystemUserAPIKey == "" && cfg.Commons != nil && cfg.Commons.SystemUserAPIKey != "" {
+		cfg.PublicSharingDrivers.JSONCS3.SystemUserAPIKey = cfg.Commons.SystemUserAPIKey
+	}
+
+	if cfg.PublicSharingDrivers.JSONCS3.SystemUserID == "" && cfg.Commons != nil && cfg.Commons.SystemUserID != "" {
+		cfg.PublicSharingDrivers.JSONCS3.SystemUserID = cfg.Commons.SystemUserID
 	}
 }
 
