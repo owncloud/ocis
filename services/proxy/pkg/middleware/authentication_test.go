46a80920446064fad6afca7dbbe98420fce21b8e
Jannik Stehle <jannik.stehle@gmail.com>
2022-09-30 09:35:36 +0200

    Fix unit tests

--- services/proxy/pkg/middleware/authentication_test.go
+++ services/proxy/pkg/middleware/authentication_test.go
@@ -13,7 +13,8 @@ var _ = Describe("authentication helpers", func() {
 		},
 		Entry("public files path", "/remote.php/dav/public-files/", true),
 		Entry("public files path without remote.php", "/remote.php/dav/public-files/", true),
-		Entry("token info path", "/remote.php/ocs/apps/files_sharing/api/v1/tokeninfo/unprotected", true),
+		Entry("token info path", "/ocs/v1.php/apps/files_sharing/api/v1/tokeninfo/unprotected", true),
+		Entry("token info path", "/ocs/v2.php/apps/files_sharing/api/v1/tokeninfo/unprotected", true),
 		Entry("capabilities", "/ocs/v1.php/cloud/capabilities", true),
 	)
 })
