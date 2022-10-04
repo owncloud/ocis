5f0e922b9973cb28d8c85e77505fa0650c585d46
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-10-04 12:56:44 +0000

    align writing of 'A comma-separated ...'
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/ocs/pkg/config/cachestore.go
+++ services/ocs/pkg/config/cachestore.go
@@ -3,6 +3,6 @@ package config
 // CacheStore defines the available configuration for the cache store
 type CacheStore struct {
 	Type    string `yaml:"type" env:"OCIS_CACHE_STORE_TYPE;OCS_CACHE_STORE_TYPE" desc:"The type of the cache store. Valid options are \"noop\", \"ocmem\", \"etcd\" and \"memory\""`
-	Address string `yaml:"address" env:"OCIS_CACHE_STORE_ADDRESS;OCS_CACHE_STORE_ADDRESS" desc:"a comma-separated list of addresses to connect to. Only for etcd"`
+	Address string `yaml:"address" env:"OCIS_CACHE_STORE_ADDRESS;OCS_CACHE_STORE_ADDRESS" desc:"A comma-separated list of addresses to connect to. Only for etcd"`
 	Size    int    `yaml:"size" env:"OCIS_CACHE_STORE_SIZE;OCS_CACHE_STORE_SIZE" desc:"Maximum size for the cache store. Only ocmem will use this option, in number of items per table. The rest will ignore the option and can grow indefinitely"`
 }
