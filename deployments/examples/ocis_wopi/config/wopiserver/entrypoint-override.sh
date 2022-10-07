e69d2e9a06d1bc1735e5096ec34b740bced48714
Willy Kloucek <wkloucek@owncloud.com>
2022-10-07 09:50:06 +0200

    update deployment examples, especially the wopi example

--- deployments/examples/ocis_wopi/config/wopiserver/entrypoint-override.sh
+++ deployments/examples/ocis_wopi/config/wopiserver/entrypoint-override.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "${WOPISECRET}" > /etc/wopi/wopisecret
@@ -6,13 +6,8 @@ echo "${WOPISECRET}" > /etc/wopi/wopisecret
 cp /etc/wopi/wopiserver.conf.dist /etc/wopi/wopiserver.conf
 sed -i 's/wopiserver.owncloud.test/'${WOPISERVER_DOMAIN}'/g' /etc/wopi/wopiserver.conf
 
-
 if [ "$WOPISERVER_INSECURE" == "true" ]; then
     sed -i 's/sslverify\s=\sTrue/sslverify = False/g' /etc/wopi/wopiserver.conf
 fi
 
-touch /var/log/wopi/wopiserver.log
-
-/app/wopiserver.py &
-
-tail -f /var/log/wopi/wopiserver.log
+/app/wopiserver.py
