26f852ae5ca964c2d135f0890670459b598c49be
Florian Schade <f.schade@icloud.com>
2022-10-14 15:53:07 +0200

    fix indexing

--- services/search/pkg/search/provider.go
+++ services/search/pkg/search/provider.go
@@ -270,8 +270,12 @@ func (p *Provider) IndexSpace(ctx context.Context, req *searchsvc.IndexSpaceRequ
 			pid = storagespace.FormatResourceID(*info.ParentId)
 		}
 		r := engine.Resource{
-			ID:       storagespace.FormatResourceID(*info.Id),
-			RootID:   storagespace.FormatResourceID(*ref.ResourceId),
+			ID: storagespace.FormatResourceID(*info.Id),
+			RootID: storagespace.FormatResourceID(provider.ResourceId{
+				StorageId: info.Id.StorageId,
+				OpaqueId:  info.Id.SpaceId,
+				SpaceId:   info.Id.SpaceId,
+			}),
 			ParentID: pid,
 			Path:     ref.Path,
 			Type:     uint64(info.Type),
