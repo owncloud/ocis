823c02b5264d081c72eb15564e239897227be424
Willy Kloucek <wkloucek@owncloud.com>
2022-10-19 14:05:06 +0200

    upgrade to go1.19 and set go mem limit

--- ocis/cmd/ocis/main.go
+++ ocis/cmd/ocis/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"os"
 
+	"github.com/KimMachineGun/automemlimit/memlimit"
 	"github.com/owncloud/ocis/v2/ocis/pkg/command"
 )
 
 func main() {
+
+	if _, present := os.LookupEnv("GOMEMLIMIT"); !present {
+		memlimit.SetGoMemLimit(0.9)
+	}
+
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
 	}
