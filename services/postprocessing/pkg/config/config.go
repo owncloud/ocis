a004095a6c81e6d30949b2eb94f27b1e191ae3f9
jkoberg <jkoberg@owncloud.com>
2022-12-08 11:00:42 +0100

    add postprocessing service
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

---
+++ services/postprocessing/pkg/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"context"
+	"time"
+
+	"github.com/owncloud/ocis/v2/ocis-pkg/shared"
+)
+
+// Config combines all available configuration parts.
+type Config struct {
+	Commons *shared.Commons `yaml:"-"` // don't use this directly as configuration for a service
+
+	Service Service `yaml:"-"`
+
+	Log *Log `yaml:"log"`
+
+	Postprocessing Postprocessing `yaml:"postprocessing"`
+
+	Context context.Context `yaml:"-"`
+}
+
+// Postprocessing definces the config options for the postprocessing service.
+type Postprocessing struct {
+	Events          Events        `yaml:"events"`
+	Virusscan       bool          `yaml:"virusscan" env:"POSTPROCESSING_VIRUSSCAN" desc:"should the system do a virusscan? Needs antivirus service"`
+	FTSIndex        bool          `yaml:"fulltextsearch" env:"POSTPROCESSING_FTS" desc:"should the system index files for fts? Needs search service"`
+	Delayprocessing time.Duration `yaml:"delayprocessing" env:"POSTPROCESSING_DELAY" desc:"the sytem sleeps for this time while postprocessing"`
+}
+
+// Events combines the configuration options for the event bus.
+type Events struct {
+	Endpoint string `yaml:"endpoint" env:"POSTPROCESSING_EVENTS_ENDPOINT" desc:"Endpoint of the event system."`
+	Cluster  string `yaml:"cluster" env:"POSTPROCESSING_EVENTS_CLUSTER" desc:"Cluster ID of the event system."`
+
+	TLSInsecure          bool   `yaml:"tls_insecure" env:"OCIS_INSECURE;SEARCH_EVENTS_TLS_INSECURE" desc:"Whether to verify the server TLS certificates."`
+	TLSRootCACertificate string `yaml:"tls_root_ca_certificate" env:"SEARCH_EVENTS_TLS_ROOT_CA_CERTIFICATE" desc:"The root CA certificate used to validate the server's TLS certificate. If provided SEARCH_EVENTS_TLS_INSECURE will be seen as false."`
+}
---
+++ services/postprocessing/pkg/postprocessing/postprocessing.go
@@ -0,0 +1,122 @@
+package postprocessing
+
+import (
+	"time"
+
+	user "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	"github.com/cs3org/reva/v2/pkg/events"
+	"github.com/owncloud/ocis/v2/services/postprocessing/pkg/config"
+)
+
+// Postprocessing handles postprocessing of a file
+type Postprocessing struct {
+	id         string
+	url        string
+	u          *user.User
+	m          map[events.Postprocessingstep]interface{}
+	filename   string
+	filesize   uint64
+	resourceId *provider.ResourceId
+	c          config.Postprocessing
+	steps      []events.Postprocessingstep
+}
+
+// New returns a new postprocessing instance
+func New(uploadID string, uploadURL string, user *user.User, filename string, filesize uint64, resourceId *provider.ResourceId, c config.Postprocessing) *Postprocessing {
+	return &Postprocessing{
+		id:         uploadID,
+		url:        uploadURL,
+		u:          user,
+		m:          make(map[events.Postprocessingstep]interface{}),
+		c:          c,
+		filename:   filename,
+		filesize:   filesize,
+		resourceId: resourceId,
+		steps:      getSteps(c),
+	}
+}
+
+// Init is the first step of the postprocessing
+func (pp *Postprocessing) Init(ev events.BytesReceived) interface{} {
+	pp.m["init"] = ev
+
+	if len(pp.steps) == 0 {
+		return pp.finished(events.PPOutcomeContinue)
+	}
+
+	return pp.nextStep(pp.steps[0])
+}
+
+// Virusscan is the virusscanning step of the postprocessing
+func (pp *Postprocessing) Virusscan(ev events.VirusscanFinished) interface{} {
+	pp.m[events.PPStepAntivirus] = ev
+
+	switch ev.Outcome {
+	case events.PPOutcomeContinue:
+		return pp.next(events.PPStepAntivirus)
+	default:
+		return pp.finished(ev.Outcome)
+
+	}
+}
+
+// Delay will sleep the configured time then continue
+func (pp *Postprocessing) Delay(ev events.StartPostprocessingStep) interface{} {
+	pp.m[events.PPStepDelay] = ev
+	time.Sleep(pp.c.Delayprocessing)
+	return pp.next(events.PPStepDelay)
+}
+
+func (pp *Postprocessing) next(current events.Postprocessingstep) interface{} {
+	l := len(pp.steps)
+	for i, s := range pp.steps {
+		if s == current && i+1 < l {
+			return pp.nextStep(pp.steps[i+1])
+		}
+	}
+	return pp.finished(events.PPOutcomeContinue)
+}
+
+func (pp *Postprocessing) nextStep(next events.Postprocessingstep) events.StartPostprocessingStep {
+	return events.StartPostprocessingStep{
+		UploadID:      pp.id,
+		URL:           pp.url,
+		ExecutingUser: pp.u,
+		Filename:      pp.filename,
+		Filesize:      pp.filesize,
+		ResourceID:    pp.resourceId,
+		StepToStart:   next,
+	}
+}
+
+func (pp *Postprocessing) finished(outcome events.PostprocessingOutcome) events.PostprocessingFinished {
+	return events.PostprocessingFinished{
+		UploadID:      pp.id,
+		Result:        pp.m,
+		ExecutingUser: pp.u,
+		Filename:      pp.filename,
+		Outcome:       outcome,
+	}
+}
+
+func getSteps(c config.Postprocessing) []events.Postprocessingstep {
+	// NOTE: first version only contains very basic configuration options
+	// But we aim for a system where postprocessing steps and their order can be configured per space
+	// ideally by the spaceadmin itself
+	// We need to iterate over configuring PP service when we see fit
+	var steps []events.Postprocessingstep
+	if c.Delayprocessing != 0 {
+		steps = append(steps, events.PPStepDelay)
+	}
+
+	if c.Virusscan {
+		steps = append(steps, events.PPStepAntivirus)
+	}
+
+	if c.FTSIndex {
+		steps = append(steps, events.PPStepFTS)
+	}
+
+	return steps
+}
