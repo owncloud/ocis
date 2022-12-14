70a6323c42967a022384554e604deed4a54460a9
Jörn Friedrich Dreyer <jfd@owncloud.com>
2022-12-14 14:04:02 +0100

    token middleware: use constant time comparison (#5225)
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>
    
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- ocis-pkg/middleware/token.go
+++ ocis-pkg/middleware/token.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"errors"
 	"net/http"
 )
@@ -12,6 +14,8 @@ var (
 
 // Token provides a middleware to check access secured by a static token.
 func Token(token string) func(http.Handler) http.Handler {
+	h := sha256.New()
+	requiredTokenHash := h.Sum(([]byte("Bearer " + token)))
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if token == "" {
@@ -26,7 +30,10 @@ func Token(token string) func(http.Handler) http.Handler {
 				return
 			}
 
-			if header != "Bearer "+token {
+			h = sha256.New()
+			providedTokenHash := h.Sum([]byte(header))
+
+			if subtle.ConstantTimeCompare(requiredTokenHash, providedTokenHash) == 0 {
 				http.Error(w, ErrInvalidToken.Error(), http.StatusUnauthorized)
 				return
 			}
