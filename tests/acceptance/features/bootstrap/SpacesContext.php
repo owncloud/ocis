ea9bf8d80bc51026500eb6aa3b6a98d814adefe3
Swikriti Tripathi <swikriti808@gmail.com>
2022-12-19 10:00:08 +0545

    adress review

--- tests/acceptance/features/bootstrap/SpacesContext.php
+++ tests/acceptance/features/bootstrap/SpacesContext.php
@@ -3091,7 +3091,7 @@ class SpacesContext implements Context {
 	}
 
 	/**
-	 * @When /^public downloads the folder "([^"]*)" of space "([^"]*)" from the last created public link of "([^"]*)" using the resource id$/
+	 * @When /^public downloads the folder "([^"]*)" of space "([^"]*)" from the last created public link of "([^"]*)"$/
 	 *
 	 * @param string $resource
 	 * @param string $space
