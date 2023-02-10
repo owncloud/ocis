337f6bf7f0475346ab90cc3b62b74f67e78ce814
Swikriti Tripathi <41103328+SwikritiT@users.noreply.github.com>
2023-02-10 16:30:07 +0545

    fix flaky test apiSpacesShares/shareSpaces.feature:42 (#5545)

--- tests/acceptance/features/bootstrap/SpacesContext.php
+++ tests/acceptance/features/bootstrap/SpacesContext.php
@@ -3241,15 +3241,14 @@ class SpacesContext implements Context {
 		$recipientType === 'user' ? $recipientId = $this->featureContext->getUserIdByUserName($recipient) : $recipientId = $this->featureContext->getGroupIdByGroupName($recipient);
 		$foundRoleInResponse = false;
 		foreach ($spaceAsArray['root']['permissions'] as $permission) {
-			$foundRoleInResponse = false;
-			if ($permission['roles'][0] === $role || $permission['grantedToIdentities'][0][$recipientType]['id'] === $recipientId) {
+			if (isset($permission['grantedToIdentities'][0][$recipientType]) && $permission['roles'][0] === $role && $permission['grantedToIdentities'][0][$recipientType]['id'] === $recipientId) {
 				$foundRoleInResponse = true;
 				if ($expirationDate !== null && isset($permission['expirationDateTime'])) {
 					Assert::assertEquals($expirationDate, (preg_split("/[\sT]+/", $permission['expirationDateTime']))[0], "$expirationDate is different in the response");
 				}
 				break;
 			}
-			Assert::assertTrue($foundRoleInResponse, "the response does not contain the $recipientType $recipient");
 		}
+		Assert::assertTrue($foundRoleInResponse, "the response does not contain the $recipientType $recipient");
 	}
 }
