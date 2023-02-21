1809ec3bc9982da8a1901625215f66ef84b3e8ab
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-02-21 10:02:33 +0000

    default to ini backend
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/storage-system/pkg/revaconfig/config.go
+++ services/storage-system/pkg/revaconfig/config.go
@@ -153,6 +153,7 @@ func StorageSystemFromStruct(cfg *config.Config) map[string]interface{} {
 func metadataDrivers(cfg *config.Config) map[string]interface{} {
 	return map[string]interface{}{
 		"ocis": map[string]interface{}{
+			"metadata_backend":           "ini",
 			"root":                       cfg.Drivers.OCIS.Root,
 			"user_layout":                "{{.Id.OpaqueId}}",
 			"treetime_accounting":        false,
