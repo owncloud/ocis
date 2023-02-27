36918959da654e1605c8c1f6cad68ad72c73a298
Artur Neumann <artur@jankaritech.com>
2023-02-27 13:44:16 +0545

    [tests-only] Cache go/bin as a long term cache (#5543)

--- Makefile
+++ Makefile
@@ -213,7 +213,7 @@ golangci-lint-fix:
 
 .PHONY: bingo-update
 bingo-update: $(BINGO)
-	$(BINGO) get -l -u
+	$(BINGO) get -l -v
 
 .PHONY: check-licenses
 check-licenses: ci-go-check-licenses ci-node-check-licenses
---
+++ tests/config/drone/check_go_bin_cache.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#
+# $1 - root path where .bingo resides
+#
+
+ROOT_PATH="$1"
+if [ -z "$1" ]; then
+    ROOT_PATH="/drone/src"
+fi
+BINGO_DIR="$ROOT_PATH/.bingo"
+
+# generate hash of a .bingo folder
+BINGO_HASH=$(cat "$BINGO_DIR"/* | sha256sum | cut -d ' ' -f 1)
+
+URL="$CACHE_ENDPOINT/$CACHE_BUCKET/ocis/go-bin/$BINGO_HASH/bin/golangci-lint"
+if curl --output /dev/null --silent --head --fail "$URL"; then
+    echo "[INFO] Go bin cache with has '$BINGO_HASH' exists."
+    # https://discourse.drone.io/t/how-to-exit-a-pipeline-early-without-failing/3951
+    # exit a Pipeline early without failing
+    exit 78
+else
+    # stored hash of a .bingo folder to '.bingo_hash' file
+    echo "$BINGO_HASH" >"$ROOT_PATH/.bingo_hash"
+    echo "[INFO] Go bin cache with has '$BINGO_HASH' does not exist."
+fi
