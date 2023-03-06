4c1915f0a07f195f5cc05e759dae6e300f6bae27
Andre Duffeck <aduffeck@users.noreply.github.com>
2023-03-06 10:02:10 +0100

    Update services/storage-system/pkg/config/config.go
    Co-authored-by: Martin <github@diemattels.at>

--- services/storage-system/pkg/config/config.go
+++ services/storage-system/pkg/config/config.go
@@ -84,7 +84,7 @@ type OCISDriver struct {
 
 // Cache holds cache config
 type Cache struct {
-	Store    string   `yaml:"store" env:"OCIS_CACHE_STORE_TYPE;STORAGE_SYSTEM_CACHE_STORE" desc:"Store implementation for the cache. Valid values are \"memory\" (default), \"redis\", and \"etcd\"."`
-	Nodes    []string `yaml:"nodes" env:"OCIS_CACHE_STORE_ADDRESS;STORAGE_SYSTEM_CACHE_NODES" desc:"Node addresses to use for the cache store."`
-	Database string   `yaml:"database" env:"STORAGE_SYSTEM_CACHE_DATABASE" desc:"Database name of the cache."`
+	Store    string   `yaml:"store" env:"OCIS_CACHE_STORE_TYPE;STORAGE_SYSTEM_CACHE_STORE" desc:"Store implementation for the cache. Supported values are 'memory' (default), 'redis', and 'etcd'. See the text description for details."`
+	Nodes    []string `yaml:"nodes" env:"OCIS_CACHE_STORE_ADDRESS;STORAGE_SYSTEM_CACHE_NODES" desc:"A comma separated list of addresses to access the configured store. This has no effect when the 'memory' store is configured. Note that the behaviour how addresses are used is dependent on the library of the configured store."`
+	Database string   `yaml:"database" env:"STORAGE_SYSTEM_CACHE_DATABASE" desc:"(optional) The database name the configured store should use. This has no effect when the 'memory' store is configured."`
 }
