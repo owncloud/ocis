bd68268dc8a5c81a5c34cc0c5a53b4464b661476
Swikriti Tripathi <swikriti808@gmail.com>
2023-03-09 12:41:32 +0545

    Refactor test for updating the share

--- tests/acceptance/features/bootstrap/Sharing.php
+++ tests/acceptance/features/bootstrap/Sharing.php
@@ -2384,7 +2384,7 @@ trait Sharing {
 	}
 
 	/**
-	 * @When the information about the last share for user :user should include
+	 * @Then the information about the last share for user :user should include
 	 *
 	 * @param string $user
 	 * @param TableNode $table
