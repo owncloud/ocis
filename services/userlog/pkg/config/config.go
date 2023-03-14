61405d1cd0669e67dee0fbad0b4e08be320a3401
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-03-14 21:37:18 +0100

    add global persistent store options to userlog config
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/userlog/pkg/config/config.go
+++ services/userlog/pkg/config/config.go
@@ -32,12 +32,12 @@ type Config struct {
 
 // Store configures the store to use
 type Store struct {
-	Type      string        `yaml:"type" env:"USERLOG_STORE_TYPE" desc:"The type of the userlog store. Supported values are: 'mem', 'ocmem', 'etcd', 'redis', 'redis-sentinel', 'nats-js', 'noop'. See the text description for details."`
-	Addresses []string      `yaml:"addresses" env:"USERLOG_STORE_ADDRESSES" desc:"A comma separated list of addresses to access the configured store. This has no effect when 'in-memory' stores are configured. Note that the behaviour how addresses are used is dependent on the library of the configured store."`
+	Type      string        `yaml:"type" env:"OCIS_PERSISTENT_STORE_TYPE;USERLOG_STORE_TYPE" desc:"The type of the userlog store. Supported values are: 'mem', 'ocmem', 'etcd', 'redis', 'redis-sentinel', 'nats-js', 'noop'. See the text description for details."`
+	Addresses []string      `yaml:"addresses" env:"OCIS_PERSISTENT_STORE_ADDRESSES;USERLOG_STORE_ADDRESSES" desc:"A comma separated list of addresses to access the configured store. This has no effect when 'in-memory' stores are configured. Note that the behaviour how addresses are used is dependent on the library of the configured store."`
 	Database  string        `yaml:"database" env:"USERLOG_STORE_DATABASE" desc:"The database name the configured store should use."`
 	Table     string        `yaml:"table" env:"USERLOG_STORE_TABLE" desc:"The database table the store should use."`
-	TTL       time.Duration `yaml:"ttl" env:"USERLOG_STORE_TTL" desc:"Time to live for events in the store. The duration can be set as number followed by a unit identifier like s, m or h. Defaults to '336h' (2 weeks)."`
-	Size      int           `yaml:"size" env:"USERLOG_STORE_SIZE" desc:"The maximum quantity of items in the store. Only applies when store type 'ocmem' is configured. Defaults to 512."`
+	TTL       time.Duration `yaml:"ttl" env:"OCIS_PERSISTENT_STORE_TTL;USERLOG_STORE_TTL" desc:"Time to live for events in the store. The duration can be set as number followed by a unit identifier like s, m or h. Defaults to '336h' (2 weeks)."`
+	Size      int           `yaml:"size" env:"OCIS_PERSISTENT_STORE_SIZE;USERLOG_STORE_SIZE" desc:"The maximum quantity of items in the store. Only applies when store type 'ocmem' is configured. Defaults to 512."`
 }
 
 // Events combines the configuration options for the event bus.
