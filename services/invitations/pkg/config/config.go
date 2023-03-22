1a7e2fd782b399de67316a485a6870e1b2dbe924
Daniël Franke <df@ponc.tech>
2023-03-22 12:51:35 +0100

    Update services/invitations/pkg/config/config.go
    Co-authored-by: Martin <github@diemattels.at>

--- services/invitations/pkg/config/config.go
+++ services/invitations/pkg/config/config.go
@@ -31,5 +31,5 @@ type Keycloak struct {
 	ClientSecret       string `yaml:"client_secret" env:"INVITATIONS_KEYCLOAK_CLIENT_SECRET" desc:"The client secret to use in authentication."`
 	ClientRealm        string `yaml:"client_realm" env:"INVITATIONS_KEYCLOAK_CLIENT_REALM" desc:"The realm the client is defined in."`
 	UserRealm          string `yaml:"user_realm" env:"INVITATIONS_KEYCLOAK_USER_REALM" desc:"The realm the users are in."`
-	InsecureSkipVerify bool   `yaml:"insecure_skip_verify" env:"INVITATIONS_KEYCLOAK_INSECURE_SKIP_VERIFY" desc:"Skip the check of the TLS certificate."`
+	InsecureSkipVerify bool   `yaml:"insecure_skip_verify" env:"INVITATIONS_KEYCLOAK_INSECURE_SKIP_VERIFY" desc:"Disable TLS certificate validation for Keycloak connections. Do not set this in production environments."`
 }
