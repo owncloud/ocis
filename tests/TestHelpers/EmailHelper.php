1f8164c527b1746cf27cbc0b4c077895d2760c36
Prajwol Amatya <83579989+PrajwolAmatya@users.noreply.github.com>
2023-05-10 14:03:52 +0545

    [tests-only] [full-ci] api test for email notification (#6145)

---
+++ tests/TestHelpers/EmailHelper.php
@@ -0,0 +1,173 @@
+<?php declare(strict_types=1);
+/**
+ * ownCloud
+ *
+ * @author Prajwol Amatya <prajwol@jankaritech.com>
+ * @copyright Copyright (c) 2023 Prajwol Amatya prajwol@jankaritech.com
+ */
+
+namespace TestHelpers;
+
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * A helper class for managing emails
+ */
+class EmailHelper {
+	/**
+	 * @param string $emailAddress
+	 *
+	 * @return string
+	 */
+	public static function getMailBoxFromEmail(string $emailAddress):string {
+		return explode("@", $emailAddress)[0];
+	}
+
+	/**
+	 * Returns the host and port where Email messages can be read and deleted
+	 * by the test runner.
+	 *
+	 * @return string
+	 */
+	public static function getLocalEmailUrl():string {
+		$localEmailHost = self::getLocalEmailHost();
+		$emailPort = \getenv('EMAIL_PORT');
+		if ($emailPort === false) {
+			$emailPort = "9000";
+		}
+		return "http://$localEmailHost:$emailPort";
+	}
+
+	/**
+	 * Returns the host name or address of the Email server as seen from the
+	 * point of view of the system-under-test.
+	 *
+	 * @return string
+	 */
+	public static function getEmailHost():string {
+		$emailHost = \getenv('EMAIL_HOST');
+		if ($emailHost === false) {
+			$emailHost = "127.0.0.1";
+		}
+		return $emailHost;
+	}
+
+	/**
+	 * Returns the host name or address of the Email server as seen from the
+	 * point of view of the test runner.
+	 *
+	 * @return string
+	 */
+	public static function getLocalEmailHost():string {
+		$localEmailHost = \getenv('LOCAL_EMAIL_HOST');
+		if ($localEmailHost === false) {
+			$localEmailHost = self::getEmailHost();
+		}
+		return $localEmailHost;
+	}
+
+	/**
+	 * Returns general response information about the provided mailbox
+	 * A mailbox is created automatically in InBucket for every unique email sender|receiver
+	 *
+	 * @param string $mailBox
+	 * @param string|null $xRequestId
+	 *
+	 * @return array
+	 * @throws GuzzleException
+	 */
+	public static function getMailBoxInformation(string $mailBox, ?string $xRequestId = null):array {
+		$response = HttpRequestHelper::get(
+			self::getLocalEmailUrl() . "/api/v1/mailbox/" . $mailBox,
+			$xRequestId,
+			null,
+			null,
+			['Content-Type' => 'application/json']
+		);
+		return \json_decode($response->getBody()->getContents());
+	}
+
+	/**
+	 * returns body content of a specific email (mailBox) with email ID (mailbox Id)
+	 *
+	 * @param string $mailBox
+	 * @param string $mailboxId
+	 * @param string|null $xRequestId
+	 *
+	 * @return object
+	 * @throws GuzzleException
+	 */
+	public static function getBodyOfAnEmailById(string $mailBox, string $mailboxId, ?string $xRequestId = null):object {
+		$response = HttpRequestHelper::get(
+			self::getLocalEmailUrl() . "/api/v1/mailbox/" . $mailBox . "/" . $mailboxId,
+			$xRequestId,
+			null,
+			null,
+			['Content-Type' => 'application/json']
+		);
+		return \json_decode($response->getBody()->getContents());
+	}
+
+	/**
+	 * Returns the body of the last received email for the provided receiver according to the provided email address and the serial number
+	 * For email number, 1 means the latest one
+	 *
+	 * @param string $emailAddress
+	 * @param string|null $xRequestId
+	 * @param int|null $emailNumber For email number, 1 means the latest one
+	 * @param int|null $waitTimeSec Time to wait for the email if the email has been delivered
+	 *
+	 * @return string
+	 * @throws GuzzleException
+	 * @throws Exception
+	 */
+	public static function getBodyOfLastEmail(
+		string $emailAddress,
+		string $xRequestId,
+		?int $emailNumber = 1,
+		?int $waitTimeSec = EMAIL_WAIT_TIMEOUT_SEC
+	):string {
+		$currentTime = \time();
+		$endTime = $currentTime + $waitTimeSec;
+		$mailBox = self::getMailBoxFromEmail($emailAddress);
+		while ($currentTime <= $endTime) {
+			$mailboxResponse = self::getMailboxInformation($mailBox);
+			if (!empty($mailboxResponse) && \sizeof($mailboxResponse) >= $emailNumber) {
+				$mailboxId = $mailboxResponse[\sizeof($mailboxResponse) - $emailNumber]->id;
+				$response = self::getBodyOfAnEmailById($mailBox, $mailboxId, $xRequestId);
+				$body = \str_replace(
+					"\r\n",
+					"\n",
+					\quoted_printable_decode($response->body->text . "\n" . $response->body->html)
+				);
+				return $body;
+			}
+			\usleep(STANDARD_SLEEP_TIME_MICROSEC * 50);
+			$currentTime = \time();
+		}
+		throw new Exception("Could not find the email to the address: " . $emailAddress);
+	}
+
+	/**
+	 * Deletes all the emails for the provided mailbox
+	 *
+	 * @param string $localInbucketUrl
+	 * @param string|null $xRequestId
+	 * @param string $mailBox
+	 *
+	 * @return ResponseInterface
+	 * @throws GuzzleException
+	 */
+	public static function deleteAllEmailsForAMailbox(
+		string $localInbucketUrl,
+		?string $xRequestId,
+		string $mailBox
+	):ResponseInterface {
+		return HttpRequestHelper::delete(
+			$localInbucketUrl . "/api/v1/mailbox/" . $mailBox,
+			$xRequestId
+		);
+	}
+}
---
+++ tests/acceptance/features/apiEmailNotification/emailNotification.feature
@@ -0,0 +1,42 @@
+@api @email
+Feature: Email notification
+  As a user
+  I want to get email notification of events related to me
+  So that I can stay updated about the events
+
+  Background:
+    Given these users have been created with default attributes and without skeleton files:
+      | username |
+      | Alice    |
+      | Brian    |
+
+
+  Scenario: a user gets an email notification when someone shares a project space
+    Given the administrator has given "Alice" the role "Space Admin" using the settings api
+    And user "Alice" has created a space "new-space" with the default quota using the GraphApi
+    When user "Alice" shares a space "new-space" with settings:
+      | shareWith | Brian  |
+      | role      | Editor |
+    Then the HTTP status code should be "200"
+    And user "Brian" should have received the following email from user "Alice" about the share of project space "new-space"
+      """
+      Hello Brian Murphy,
+
+      %displayname% has invited you to join "new-space".
+
+      Click here to view it: %base_url%/f/%space_id%
+      """
+
+
+  Scenario: a user gets an email notification when someone shares a file
+    Given user "Alice" has uploaded file with content "sample text" to "lorem.txt"
+    When user "Alice" has shared file "lorem.txt" with user "Brian" with permissions "17"
+    Then the HTTP status code should be "200"
+    And user "Brian" should have received the following email from user "Alice"
+      """
+      Hello Brian Murphy
+
+      %displayname% has shared "lorem.txt" with you.
+
+      Click here to view it: %base_url%/files/shares/with-me
+      """
