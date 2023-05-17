ec14d1bf519ba23f8876d3e8153bdb0bc040575f
Ralf Haferkamp <rhaferkamp@owncloud.com>
2023-05-17 10:57:49 +0200

    idp/proxy: Match users by ID instead of name by default
    Reconfigure the oidc clients for lico, so that lico adds the "lg.uuid" to
    tokens and userinfo by default. That claim will contain the userid. So
    we can now use the userid for matching users when using the default
    idm/idp configuration. This fixes further problems so that users being
    recreated with the same name are correctly treated as differnt users.
    
    Fixes: #904

--- services/idp/pkg/config/config.go
+++ services/idp/pkg/config/config.go
@@ -61,6 +61,7 @@ type Client struct {
 	ID              string   `yaml:"id"`
 	Name            string   `yaml:"name"`
 	Trusted         bool     `yaml:"trusted"`
+	ImplicitScopes  []string `yaml:"implicit_scopes"`
 	Secret          string   `yaml:"secret"`
 	RedirectURIs    []string `yaml:"redirect_uris"`
 	Origins         []string `yaml:"origins"`
