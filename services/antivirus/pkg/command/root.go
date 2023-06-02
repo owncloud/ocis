d347448ec02396e79cf3535f47f4c2bdc0f8df6b
jkoberg <jkoberg@owncloud.com>
2023-06-02 12:01:17 +0200

    remove duplicated sutureservice code
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- services/antivirus/pkg/command/root.go
+++ services/antivirus/pkg/command/root.go
@@ -1,13 +1,10 @@
 package command
 
 import (
-	"context"
 	"os"
 
 	"github.com/owncloud/ocis/v2/ocis-pkg/clihelper"
-	ociscfg "github.com/owncloud/ocis/v2/ocis-pkg/config"
 	"github.com/owncloud/ocis/v2/services/antivirus/pkg/config"
-	"github.com/thejerf/suture/v4"
 	"github.com/urfave/cli/v2"
 )
 
@@ -29,26 +26,4 @@ func Execute(cfg *config.Config) error {
 	})
 
 	return app.Run(os.Args)
-}
-
-// SutureService allows for the web command to be embedded and supervised by a suture supervisor tree.
-type SutureService struct {
-	cfg *config.Config
-}
-
-// NewSutureService creates a new web.SutureService
-func NewSutureService(cfg *ociscfg.Config) suture.Service {
-	cfg.Policies.Commons = cfg.Commons
-	return SutureService{
-		cfg: cfg.Antivirus,
-	}
-}
-
-func (s SutureService) Serve(ctx context.Context) error {
-	s.cfg.Context = ctx
-	if err := Execute(s.cfg); err != nil {
-		return err
-	}
-
-	return nil
 }
--- services/graph/pkg/command/root.go
+++ services/graph/pkg/command/root.go
@@ -1,13 +1,10 @@
 package command
 
 import (
-	"context"
 	"os"
 
 	"github.com/owncloud/ocis/v2/ocis-pkg/clihelper"
-	"github.com/thejerf/suture/v4"
 
-	ociscfg "github.com/owncloud/ocis/v2/ocis-pkg/config"
 	"github.com/owncloud/ocis/v2/services/graph/pkg/config"
 	"github.com/urfave/cli/v2"
 )
@@ -34,26 +31,4 @@ func Execute(cfg *config.Config) error {
 		Commands: GetCommands(cfg),
 	})
 	return app.Run(os.Args)
-}
-
-// SutureService allows for the graph command to be embedded and supervised by a suture supervisor tree.
-type SutureService struct {
-	cfg *config.Config
-}
-
-// NewSutureService creates a new graph.SutureService
-func NewSutureService(cfg *ociscfg.Config) suture.Service {
-	cfg.Graph.Commons = cfg.Commons
-	return SutureService{
-		cfg: cfg.Graph,
-	}
-}
-
-func (s SutureService) Serve(ctx context.Context) error {
-	s.cfg.Context = ctx
-	if err := Execute(s.cfg); err != nil {
-		return err
-	}
-
-	return nil
 }
