07419aedbc0742674b611d23eb56358fdb8dff6e
Viktor Scharf <scharf.vi@gmail.com>
2023-06-27 09:46:49 +0200

    fix after review

--- tests/acceptance/features/bootstrap/Sharing.php
+++ tests/acceptance/features/bootstrap/Sharing.php
@@ -2807,7 +2807,6 @@ trait Sharing {
 		$this->setLastPublicShareData($this->getResponseXml(null, __METHOD__));
 		foreach ($bodyRows as $field => $value) {
 			if (\in_array($field, $userRelatedFieldNames)) {
-				$value = $this->substituteInLineCodes($value, $user);
 				$value = $this->substituteInLineCodes(
 					$value,
 					$user,
