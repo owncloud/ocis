ea39236e0e8bc5a3300452633eba63e3121f2db9
Daniël Franke <df@ponc.tech>
2023-07-06 11:29:38 +0200

    Move app-registry service to service tracer.
    This changes the app-registry service away from using global tracers,
    which makes debugging tracing issues easier going forward.

--- services/app-registry/pkg/command/server.go
+++ services/app-registry/pkg/command/server.go
@@ -11,13 +11,13 @@ import (
 	"github.com/oklog/run"
 	"github.com/owncloud/ocis/v2/ocis-pkg/config/configlog"
 	"github.com/owncloud/ocis/v2/ocis-pkg/registry"
+	"github.com/owncloud/ocis/v2/ocis-pkg/tracing"
 	"github.com/owncloud/ocis/v2/ocis-pkg/version"
 	"github.com/owncloud/ocis/v2/services/app-registry/pkg/config"
 	"github.com/owncloud/ocis/v2/services/app-registry/pkg/config/parser"
 	"github.com/owncloud/ocis/v2/services/app-registry/pkg/logging"
 	"github.com/owncloud/ocis/v2/services/app-registry/pkg/revaconfig"
 	"github.com/owncloud/ocis/v2/services/app-registry/pkg/server/debug"
-	"github.com/owncloud/ocis/v2/services/app-registry/pkg/tracing"
 	"github.com/urfave/cli/v2"
 )
 
@@ -32,7 +32,7 @@ func Server(cfg *config.Config) *cli.Command {
 		},
 		Action: func(c *cli.Context) error {
 			logger := logging.Configure(cfg.Service.Name, cfg.Log)
-			err := tracing.Configure(cfg, logger)
+			tracingProvider, err := tracing.GetServiceTraceProvider(cfg.Tracing, cfg.Service.Name)
 			if err != nil {
 				return err
 			}
@@ -49,6 +49,7 @@ func Server(cfg *config.Config) *cli.Command {
 				runtime.RunWithOptions(rCfg, pidFile,
 					runtime.WithLogger(&logger.Logger),
 					runtime.WithRegistry(reg),
+					runtime.WithTraceProvider(tracingProvider),
 				)
 
 				return nil
@@ -66,7 +67,6 @@ func Server(cfg *config.Config) *cli.Command {
 				debug.Context(ctx),
 				debug.Config(cfg),
 			)
-
 			if err != nil {
 				logger.Info().Err(err).Str("server", "debug").Msg("Failed to initialize server")
 				return err
