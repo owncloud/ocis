d6f45f815639fa813e3c83494ad295cfc5ef5466
Christian Richter <crichter@owncloud.com>
2023-07-27 16:05:05 +0200

    introduce registry
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- ocis-pkg/registry/registry.go
+++ ocis-pkg/registry/registry.go
@@ -3,82 +3,52 @@ package registry
 import (
 	"os"
 	"strings"
-	"sync"
 	"time"
 
-	rRegistry "github.com/cs3org/reva/v2/pkg/registry"
 	consulr "github.com/go-micro/plugins/v4/registry/consul"
 	etcdr "github.com/go-micro/plugins/v4/registry/etcd"
 	kubernetesr "github.com/go-micro/plugins/v4/registry/kubernetes"
 	mdnsr "github.com/go-micro/plugins/v4/registry/mdns"
-	memr "github.com/go-micro/plugins/v4/registry/memory"
 	natsr "github.com/go-micro/plugins/v4/registry/nats"
-	mRegistry "go-micro.dev/v4/registry"
+
+	"go-micro.dev/v4/registry"
 	"go-micro.dev/v4/registry/cache"
 )
 
-const (
+var (
 	registryEnv        = "MICRO_REGISTRY"
 	registryAddressEnv = "MICRO_REGISTRY_ADDRESS"
 )
 
-var (
-	once      sync.Once
-	regPlugin string
-	reg       mRegistry.Registry
-)
-
-func Configure(plugin string) {
-	if reg == nil {
-		regPlugin = plugin
-	}
-}
-
 // GetRegistry returns a configured micro registry based on Micro env vars.
 // It defaults to mDNS, so mind that systems with mDNS disabled by default (i.e SUSE) will have a hard time
 // and it needs to explicitly use etcd. Os awareness for providing a working registry out of the box should be done.
-func GetRegistry() mRegistry.Registry {
-	once.Do(func() {
-		addresses := strings.Split(os.Getenv(registryAddressEnv), ",")
-		// prefer env of setting from Configure()
-		plugin := os.Getenv(registryEnv)
-		if plugin == "" {
-			plugin = regPlugin
-		}
+func GetRegistry() registry.Registry {
+	addresses := strings.Split(os.Getenv(registryAddressEnv), ",")
 
-		switch plugin {
-		case "nats":
-			reg = natsr.NewRegistry(
-				mRegistry.Addrs(addresses...),
-				natsr.RegisterAction("put"),
-			)
-		case "kubernetes":
-			reg = kubernetesr.NewRegistry(
-				mRegistry.Addrs(addresses...),
-			)
-		case "etcd":
-			reg = etcdr.NewRegistry(
-				mRegistry.Addrs(addresses...),
-			)
-		case "consul":
-			reg = consulr.NewRegistry(
-				mRegistry.Addrs(addresses...),
-			)
-		case "memory":
-			reg = memr.NewRegistry()
-		default:
-			reg = mdnsr.NewRegistry()
-		}
+	var r registry.Registry
+	switch os.Getenv(registryEnv) {
+	case "nats":
+		r = natsr.NewRegistry(
+			registry.Addrs(addresses...),
+		)
+	case "kubernetes":
+		r = kubernetesr.NewRegistry(
+			registry.Addrs(addresses...),
+		)
+	case "etcd":
+		r = etcdr.NewRegistry(
+			registry.Addrs(addresses...),
+		)
+	case "consul":
+		r = consulr.NewRegistry(
+			registry.Addrs(addresses...),
+		)
+	default:
+		r = mdnsr.NewRegistry()
+	}
 
-		// No cache needed for in-memory registry
-		if plugin != "memory" {
-			reg = cache.New(reg, cache.WithTTL(30*time.Second))
-		}
-
-		// fixme: lazy initialization of reva registry, needs refactor to a explicit call per service
-		_ = rRegistry.Init(reg)
-	})
 	// always use cached registry to prevent registry
 	// lookup for every request
-	return reg
+	return cache.New(r, cache.WithTTL(20*time.Second))
 }
