14cd33f8c8d1fb394b8106db387b1a9537a4164d
Jörn Friedrich Dreyer <jfd@butonic.de>
2023-08-07 15:47:46 +0200

    log error in cli
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- ocis/cmd/ocis/main.go
+++ ocis/cmd/ocis/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/owncloud/ocis/v2/ocis/pkg/command"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	if err := command.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
