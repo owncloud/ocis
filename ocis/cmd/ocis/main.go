2422973fee16f6245c6ee75ca16209f51ae95fef
Jörn Friedrich Dreyer <jfd@owncloud.com>
2023-08-08 11:55:19 +0200

    log error in cli (#6976)
    * log error in cli
    
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>
    
    * write to stderr
    
    * Update ocis/cmd/ocis/main.go
    
    Co-authored-by: Andre Duffeck <aduffeck@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>
    Co-authored-by: Andre Duffeck <aduffeck@users.noreply.github.com>

--- ocis/cmd/ocis/main.go
+++ ocis/cmd/ocis/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/owncloud/ocis/v2/ocis/pkg/command"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	if err := command.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 }
