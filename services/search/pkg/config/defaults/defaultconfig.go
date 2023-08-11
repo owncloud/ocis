d8c2957c84841cb4fa461d9525bc82ad4e3df0bb
jkoberg <jkoberg@owncloud.com>
2023-08-11 14:10:10 +0200

    use service accounts for search
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- services/search/pkg/config/defaults/defaultconfig.go
+++ services/search/pkg/config/defaults/defaultconfig.go
@@ -54,7 +54,10 @@ func DefaultConfig() *config.Config {
 			EnableTLS:        false,
 		},
 		ContentExtractionSizeLimit: 20 * 1024 * 1024, // Limit content extraction to <20MB files by default
-		MachineAuthAPIKey:          "",
+		ServiceAccount: config.ServiceAccount{
+			ServiceAccountID:     "service-user-id",
+			ServiceAccountSecret: "secret-string",
+		},
 	}
 }
 
@@ -89,10 +92,6 @@ func EnsureDefaults(cfg *config.Config) {
 		}
 	} else if cfg.TokenManager == nil {
 		cfg.TokenManager = &config.TokenManager{}
-	}
-
-	if cfg.MachineAuthAPIKey == "" && cfg.Commons != nil && cfg.Commons.MachineAuthAPIKey != "" {
-		cfg.MachineAuthAPIKey = cfg.Commons.MachineAuthAPIKey
 	}
 
 	if cfg.Reva == nil && cfg.Commons != nil {
