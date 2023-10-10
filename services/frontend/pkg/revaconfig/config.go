ff4720863a4a09312a8a460679160118442c9f89
Ralf Haferkamp <rhaferkamp@owncloud.com>
2023-10-10 14:31:38 +0200

    Fix reva config of frontend service
    Set an empty Credentials chain. In ocis all non-reva token authentication is
    handled by the proxy. This avoid irritating error messages about the missing
    'auth-bearer' service.
    
    Fixes: #6692
    (cherry picked from commit e316d8ea3a94242d887a33431a7fcba36c40db73)

--- services/frontend/pkg/revaconfig/config.go
+++ services/frontend/pkg/revaconfig/config.go
@@ -94,7 +94,6 @@ func FrontendConfigFromStruct(cfg *config.Config) (map[string]interface{}, error
 				},
 				"auth": map[string]interface{}{
 					"credentials_by_user_agent": cfg.Middleware.Auth.CredentialsByUserAgent,
-					"credential_chain":          []string{"bearer"},
 				},
 				"prometheus": map[string]interface{}{
 					"namespace": "ocis",
