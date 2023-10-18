c8f487d8042ad06360b1dd19854bc185676d1602
jkoberg <jkoberg@owncloud.com>
2023-10-18 10:48:36 +0200

    bump go-micro nats-js store
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- vendor/github.com/go-micro/plugins/v4/store/nats-js/options.go
+++ vendor/github.com/go-micro/plugins/v4/store/nats-js/options.go
@@ -7,7 +7,7 @@ import (
 	"go-micro.dev/v4/store"
 )
 
-// store.Option
+// store.Option.
 type natsOptionsKey struct{}
 type jsOptionsKey struct{}
 type objOptionsKey struct{}
@@ -15,15 +15,15 @@ type ttlOptionsKey struct{}
 type memoryOptionsKey struct{}
 type descriptionOptionsKey struct{}
 
-// store.DeleteOption
+// store.DeleteOption.
 type delBucketOptionsKey struct{}
 
-// NatsOptions accepts nats.Options
+// NatsOptions accepts nats.Options.
 func NatsOptions(opts nats.Options) store.Option {
 	return setStoreOption(natsOptionsKey{}, opts)
 }
 
-// JetStreamOptions accepts multiple nats.JSOpt
+// JetStreamOptions accepts multiple nats.JSOpt.
 func JetStreamOptions(opts ...nats.JSOpt) store.Option {
 	return setStoreOption(jsOptionsKey{}, opts)
 }
@@ -35,34 +35,42 @@ func ObjectStoreOptions(cfg ...*nats.ObjectStoreConfig) store.Option {
 }
 
 // DefaultTTL sets the default TTL to use for new buckets
-//  By default no TTL is set.
+//
+//	By default no TTL is set.
 //
 // TTL ON INDIVIDUAL WRITE CALLS IS NOT SUPPORTED, only bucket wide TTL.
 // Either set a default TTL with this option or provide bucket specific options
-//  with ObjectStoreOptions
+//
+//	with ObjectStoreOptions
 func DefaultTTL(ttl time.Duration) store.Option {
 	return setStoreOption(ttlOptionsKey{}, ttl)
 }
 
 // DefaultMemory sets the default storage type to memory only.
 //
-//  The default is file storage, persisting storage between service restarts.
+//	The default is file storage, persisting storage between service restarts.
+//
 // Be aware that the default storage location of NATS the /tmp dir is, and thus
-//  won't persist reboots.
+//
+//	won't persist reboots.
 func DefaultMemory() store.Option {
 	return setStoreOption(memoryOptionsKey{}, nats.MemoryStorage)
 }
 
 // DefaultDescription sets the default description to use when creating new
-//  buckets. The default is "Store managed by go-micro"
+//
+//	buckets. The default is "Store managed by go-micro"
 func DefaultDescription(text string) store.Option {
 	return setStoreOption(descriptionOptionsKey{}, text)
 }
 
 // DeleteBucket will use the key passed to Delete as a bucket (database) name,
-//  and delete the bucket.
+//
+//	and delete the bucket.
+//
 // This option should not be combined with the store.DeleteFrom option, as
-//  that will overwrite the delete action.
+//
+//	that will overwrite the delete action.
 func DeleteBucket() store.DeleteOption {
 	return func(d *store.DeleteOptions) {
 		d.Table = "DELETE_BUCKET"
