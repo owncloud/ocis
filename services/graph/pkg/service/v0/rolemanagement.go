bcc07e70a63b1a51535d6a48733a79bd458a872e
Roman Perekhod <rperekhod@owncloud.com>
2023-10-19 19:12:56 +0200

    implement sharing roles

---
+++ services/graph/pkg/service/v0/rolemanagement.go
@@ -0,0 +1,61 @@
+package svc
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/cs3org/reva/v2/pkg/conversions"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	libregraph "github.com/owncloud/libre-graph-api-go"
+	"github.com/owncloud/ocis/v2/services/graph/pkg/service/v0/errorcode"
+)
+
+// GetRoleDefinitions a list of permission roles than can be used when sharing with users or groups
+func (g Graph) GetRoleDefinitions(w http.ResponseWriter, r *http.Request) {
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, getRoleDefinitionList(g.config.FilesSharing.EnableResharing))
+}
+
+// GetRoleDefinition a permission role than can be used when sharing with users or groups
+func (g Graph) GetRoleDefinition(w http.ResponseWriter, r *http.Request) {
+	logger := g.logger.SubloggerWithRequestID(r.Context())
+	roleID, err := url.PathUnescape(chi.URLParam(r, "roleID"))
+	if err != nil {
+		logger.Debug().Err(err).Str("roleID", chi.URLParam(r, "roleID")).Msg("could not get roleID: unescaping is failed")
+		errorcode.InvalidRequest.Render(w, r, http.StatusBadRequest, "unescaping role id failed")
+		return
+	}
+	role, err := getRoleDefinition(roleID, g.config.FilesSharing.EnableResharing)
+	if err != nil {
+		logger.Debug().Str("roleID", roleID).Msg("could not get role: not found")
+		errorcode.ItemNotFound.Render(w, r, http.StatusNotFound, err.Error())
+		return
+	}
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, role)
+}
+
+func getRoleDefinitionList(resharing bool) []*libregraph.UnifiedRoleDefinition {
+	return []*libregraph.UnifiedRoleDefinition{
+		conversions.NewViewerUnifiedRole(resharing),
+		conversions.NewSpaceViewerUnifiedRole(),
+		conversions.NewEditorUnifiedRole(resharing),
+		conversions.NewSpaceEditorUnifiedRole(),
+		conversions.NewFileEditorUnifiedRole(),
+		conversions.NewCoownerUnifiedRole(),
+		conversions.NewUploaderUnifiedRole(),
+		conversions.NewManagerUnifiedRole(),
+	}
+}
+
+func getRoleDefinition(roleID string, resharing bool) (*libregraph.UnifiedRoleDefinition, error) {
+	roleList := getRoleDefinitionList(resharing)
+	for _, role := range roleList {
+		if role != nil && role.Id != nil && *role.Id == roleID {
+			return role, nil
+		}
+	}
+	return nil, fmt.Errorf("role not found")
+}
