f31906da4769b4b78aeb4ec799fd0aebc77426b1
Benedikt Kulmann <benedikt@kulmann.biz>
2023-11-30 10:57:44 +0100

    feat: add and adjust configs for embed mode

--- services/web/pkg/config/defaults/defaultconfig.go
+++ services/web/pkg/config/defaults/defaultconfig.go
@@ -201,7 +201,9 @@ func Sanitize(cfg *config.Config) {
 	// remove Embed parent if no value is set
 	if cfg.Web.Config.Options.Embed.Enabled == "" &&
 		cfg.Web.Config.Options.Embed.Target == "" &&
-		cfg.Web.Config.Options.Embed.MessagesOrigin == "" {
+		cfg.Web.Config.Options.Embed.MessagesOrigin == "" &&
+		cfg.Web.Config.Options.Embed.DelegateAuthentication == "" &&
+		cfg.Web.Config.Options.Embed.DelegateAuthenticationOrigin == "" {
 		cfg.Web.Config.Options.Embed = nil
 	}
 }
