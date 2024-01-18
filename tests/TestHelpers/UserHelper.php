c5f768153ad773f3ff39dbad3b81819c4959ae56
Nalem7 <61624650+nabim777@users.noreply.github.com>
2024-01-18 16:58:51 +0545

    [tests-only][full-ci] Restructure the steps implementation for group delete to use graph API endpoint (#8006) (#8227)
    * remove ocs endpoint from delete group
    
    * remove adminHasDeletedGroupUsingTheGraphApi
    
    * adressing reviews

--- tests/TestHelpers/UserHelper.php
+++ tests/TestHelpers/UserHelper.php
@@ -191,68 +191,6 @@ class UserHelper {
 	/**
 	 *
 	 * @param string|null $baseUrl
-	 * @param string|null $group
-	 * @param string|null $adminUser
-	 * @param string|null $adminPassword
-	 * @param string|null $xRequestId
-	 *
-	 * @return ResponseInterface
-	 * @throws GuzzleException
-	 */
-	public static function createGroup(
-		?string $baseUrl,
-		?string $group,
-		?string $adminUser,
-		?string $adminPassword,
-		?string $xRequestId = ''
-	):ResponseInterface {
-		return OcsApiHelper::sendRequest(
-			$baseUrl,
-			$adminUser,
-			$adminPassword,
-			"POST",
-			"/cloud/groups",
-			$xRequestId,
-			['groupid' => $group]
-		);
-	}
-
-	/**
-	 *
-	 * @param string|null $baseUrl
-	 * @param string|null $group
-	 * @param string|null $adminUser
-	 * @param string|null $adminPassword
-	 * @param string|null $xRequestId
-	 * @param int|null $ocsApiVersion
-	 *
-	 * @return ResponseInterface
-	 * @throws GuzzleException
-	 */
-	public static function deleteGroup(
-		?string $baseUrl,
-		?string $group,
-		?string $adminUser,
-		?string $adminPassword,
-		?string $xRequestId = '',
-		?int $ocsApiVersion = 2
-	):ResponseInterface {
-		$group = \rawurlencode($group);
-		return OcsApiHelper::sendRequest(
-			$baseUrl,
-			$adminUser,
-			$adminPassword,
-			"DELETE",
-			"/cloud/groups/" . $group,
-			$xRequestId,
-			[],
-			$ocsApiVersion
-		);
-	}
-
-	/**
-	 *
-	 * @param string|null $baseUrl
 	 * @param string|null $user
 	 * @param string|null $group
 	 * @param string|null $adminUser
