48610091ad0fc3109b8d866281bf05552fb68ebd
Jörn Friedrich Dreyer <jfd@owncloud.com>
2024-01-25 22:00:12 +0100

    set size when starting an upload session via graph (#8288)
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/graph/pkg/service/v0/driveitems.go
+++ services/graph/pkg/service/v0/driveitems.go
@@ -94,10 +94,13 @@ func (g Graph) CreateUploadSession(w http.ResponseWriter, r *http.Request) {
 	if cusr.Item.Name != "" {
 		ref.Path = utils.MakeRelativePath(cusr.Item.Name)
 	}
-	// TODO size?
+	req := &storageprovider.InitiateFileUploadRequest{
+		Ref:    ref,
+		Opaque: utils.AppendPlainToOpaque(nil, "Upload-Length", strconv.FormatUint(uint64(cusr.Item.FileSize), 10)),
+	}
 
 	ctx := r.Context()
-	res, err := gatewayClient.InitiateFileUpload(ctx, &storageprovider.InitiateFileUploadRequest{Ref: ref})
+	res, err := gatewayClient.InitiateFileUpload(ctx, req)
 	switch {
 	case err != nil:
 		errorcode.GeneralException.Render(w, r, http.StatusInternalServerError, err.Error())
