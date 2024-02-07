2b667003b5509de2de84d351251e5061d182bb35
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2024-02-07 17:13:28 +0100

    fix: properly check expiry and verify signature of signed urls (#8383)
    fix: signed url expiry validation only checks for expiry and not for used before

--- services/proxy/pkg/middleware/signed_url_auth_test.go
+++ services/proxy/pkg/middleware/signed_url_auth_test.go
@@ -1,6 +1,14 @@
 package middleware
 
 import (
+	"context"
+	userpb "github.com/cs3org/go-cs3apis/cs3/identity/user/v1beta1"
+	revactx "github.com/cs3org/reva/v2/pkg/ctx"
+	storemsg "github.com/owncloud/ocis/v2/protogen/gen/ocis/messages/store/v0"
+	v0 "github.com/owncloud/ocis/v2/protogen/gen/ocis/services/store/v0"
+	"github.com/owncloud/ocis/v2/services/proxy/pkg/config"
+	"github.com/stretchr/testify/assert"
+	"go-micro.dev/v4/client"
 	"net/http/httptest"
 	"testing"
 	"time"
@@ -34,21 +42,23 @@ func TestSignedURLAuth_allRequiredParametersPresent(t *testing.T) {
 	pua := SignedURLAuthenticator{}
 	baseURL := "https://example.com/example.jpg?"
 	tests := []struct {
-		params   string
-		expected bool
+		params       string
+		errorMessage string
 	}{
-		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Expires=something&OC-Verb=something", true},
-		{"OC-Credential=something&OC-Date=something&OC-Expires=something&OC-Verb=something", false},
-		{"OC-Signature=something&OC-Date=something&OC-Expires=something&OC-Verb=something", false},
-		{"OC-Signature=something&OC-Credential=something&OC-Expires=something&OC-Verb=something", false},
-		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Verb=something", false},
-		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Expires=something", false},
+		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Expires=something&OC-Verb=something", ""},
+		{"OC-Credential=something&OC-Date=something&OC-Expires=something&OC-Verb=something", "required OC-Signature parameter not found"},
+		{"OC-Signature=something&OC-Date=something&OC-Expires=something&OC-Verb=something", "required OC-Credential parameter not found"},
+		{"OC-Signature=something&OC-Credential=something&OC-Expires=something&OC-Verb=something", "required OC-Date parameter not found"},
+		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Verb=something", "required OC-Expires parameter not found"},
+		{"OC-Signature=something&OC-Credential=something&OC-Date=something&OC-Expires=something", "required OC-Verb parameter not found"},
 	}
 	for _, tt := range tests {
 		r := httptest.NewRequest("", baseURL+tt.params, nil)
-		ok, _ := pua.allRequiredParametersArePresent(r.URL.Query())
-		if ok != tt.expected {
-			t.Errorf("with %s expected %t got %t", tt.params, tt.expected, ok)
+		err := pua.allRequiredParametersArePresent(r.URL.Query())
+		if tt.errorMessage != "" {
+			assert.EqualError(t, err, tt.errorMessage, tt.params)
+		} else {
+			assert.Nil(t, err)
 		}
 	}
 }
@@ -56,20 +66,22 @@ func TestSignedURLAuth_allRequiredParametersPresent(t *testing.T) {
 func TestSignedURLAuth_requestMethodMatches(t *testing.T) {
 	pua := SignedURLAuthenticator{}
 	tests := []struct {
-		method   string
-		url      string
-		expected bool
+		method       string
+		url          string
+		errorMessage string
 	}{
-		{"GET", "https://example.com/example.jpg?OC-Verb=GET", true},
-		{"GET", "https://example.com/example.jpg?OC-Verb=get", true},
-		{"POST", "https://example.com/example.jpg?OC-Verb=GET", false},
+		{"GET", "https://example.com/example.jpg?OC-Verb=GET", ""},
+		{"GET", "https://example.com/example.jpg?OC-Verb=get", ""},
+		{"POST", "https://example.com/example.jpg?OC-Verb=GET", "required OC-Verb parameter did not match request method"},
 	}
 
 	for _, tt := range tests {
 		r := httptest.NewRequest(tt.method, tt.url, nil)
-		ok, _ := pua.requestMethodMatches(r.Method, r.URL.Query())
-		if ok != tt.expected {
-			t.Errorf("with method %s and url %s expected %t got %t", tt.method, tt.url, tt.expected, ok)
+		err := pua.requestMethodMatches(r.Method, r.URL.Query())
+		if tt.errorMessage != "" {
+			assert.EqualError(t, err, tt.errorMessage, tt.url)
+		} else {
+			assert.Nil(t, err)
 		}
 	}
 }
@@ -77,50 +89,62 @@ func TestSignedURLAuth_requestMethodMatches(t *testing.T) {
 func TestSignedURLAuth_requestMethodIsAllowed(t *testing.T) {
 	pua := SignedURLAuthenticator{}
 	tests := []struct {
-		method   string
-		allowed  []string
-		expected bool
+		method       string
+		allowed      []string
+		errorMessage string
 	}{
-		{"GET", []string{}, false},
-		{"GET", []string{"POST"}, false},
-		{"GET", []string{"GET"}, true},
-		{"GET", []string{"get"}, true},
-		{"GET", []string{"POST", "GET"}, true},
+		{"GET", []string{}, "request method is not listed in PreSignedURLConfig AllowedHTTPMethods"},
+		{"GET", []string{"POST"}, "request method is not listed in PreSignedURLConfig AllowedHTTPMethods"},
+		{"GET", []string{"GET"}, ""},
+		{"GET", []string{"get"}, ""},
+		{"GET", []string{"POST", "GET"}, ""},
 	}
 
 	for _, tt := range tests {
 		pua.PreSignedURLConfig.AllowedHTTPMethods = tt.allowed
-		ok, _ := pua.requestMethodIsAllowed(tt.method)
-
-		if ok != tt.expected {
-			t.Errorf("with method %s and allowed methods %s expected %t got %t", tt.method, tt.allowed, tt.expected, ok)
+		err := pua.requestMethodIsAllowed(tt.method)
+		if tt.errorMessage != "" {
+			assert.EqualError(t, err, tt.errorMessage)
+		} else {
+			assert.Nil(t, err)
 		}
 	}
 }
 
 func TestSignedURLAuth_urlIsExpired(t *testing.T) {
-	pua := SignedURLAuthenticator{}
 	nowFunc := func() time.Time {
 		t, _ := time.Parse(time.RFC3339, "2020-02-02T12:30:00.000Z")
 		return t
 	}
+	pua := SignedURLAuthenticator{
+		Now: nowFunc,
+	}
 
 	tests := []struct {
-		url       string
-		isExpired bool
+		url          string
+		errorMessage string
 	}{
-		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=61", false},
-		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=invalid", true},
-		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=59", true},
-		{"http://example.com/example.jpg?OC-Date=2020-02-03T12:29:00.000Z&OC-Expires=59", true},
-		{"http://example.com/example.jpg?OC-Date=2020-02-01T12:29:00.000Z&OC-Expires=59", true},
+		// a valid signed url
+		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=61", ""},
+		// invalid expiry
+		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=invalid", "time: invalid duration \"invalids\""},
+		// wrong date format on OC-Date
+		{"http://example.com/example.jpg?OC-Date=2020-02-02TTT12:29:00.000Z&OC-Expires=5", "parsing time \"2020-02-02TTT12:29:00.000Z\" as \"2006-01-02T15:04:05Z07:00\": cannot parse \"TT12:29:00.000Z\" as \"15\""},
+		// expired - 12:29:00 + 59s < 12:30
+		{"http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=59", "URL is expired"},
+		// expired - basically url was created yesterday
+		{"http://example.com/example.jpg?OC-Date=2020-02-01T12:29:00.000Z&OC-Expires=59", "URL is expired"},
+		// future OC-Date - also valid now
+		{"http://example.com/example.jpg?OC-Date=2020-02-03T12:29:00.000Z&OC-Expires=59", ""},
 	}
 
 	for _, tt := range tests {
 		r := httptest.NewRequest("", tt.url, nil)
-		expired, _ := pua.urlIsExpired(r.URL.Query(), nowFunc)
-		if expired != tt.isExpired {
-			t.Errorf("with %s expected %t got %t", tt.url, tt.isExpired, expired)
+		err := pua.urlIsExpired(r.URL.Query())
+		if tt.errorMessage != "" {
+			assert.EqualError(t, err, tt.errorMessage, tt.url)
+		} else {
+			assert.Nil(t, err)
 		}
 	}
 }
@@ -132,5 +156,87 @@ func TestSignedURLAuth_createSignature(t *testing.T) {
 
 	if s != expected {
 		t.Fail()
+	}
+}
+
+type MyStoreService struct {
+}
+
+func (s MyStoreService) Read(_ context.Context, _ *v0.ReadRequest, _ ...client.CallOption) (*v0.ReadResponse, error) {
+	r := v0.ReadResponse{
+		Records: []*storemsg.Record{{
+			Key:      "foo",
+			Value:    []byte("1234567890"),
+			Expiry:   0,
+			Metadata: nil,
+		}},
+	}
+	return &r, nil
+}
+func (s MyStoreService) Write(_ context.Context, _ *v0.WriteRequest, _ ...client.CallOption) (*v0.WriteResponse, error) {
+	return nil, nil
+}
+func (s MyStoreService) Delete(_ context.Context, _ *v0.DeleteRequest, _ ...client.CallOption) (*v0.DeleteResponse, error) {
+	return nil, nil
+}
+func (s MyStoreService) List(_ context.Context, _ *v0.ListRequest, _ ...client.CallOption) (v0.Store_ListService, error) {
+	return nil, nil
+}
+func (s MyStoreService) Databases(_ context.Context, _ *v0.DatabasesRequest, _ ...client.CallOption) (*v0.DatabasesResponse, error) {
+	return nil, nil
+}
+func (s MyStoreService) Tables(_ context.Context, _ *v0.TablesRequest, _ ...client.CallOption) (*v0.TablesResponse, error) {
+	return nil, nil
+}
+
+func TestSignedURLAuth_validate(t *testing.T) {
+	nowFunc := func() time.Time {
+		t, _ := time.Parse(time.RFC3339, "2020-02-02T12:30:00.000Z")
+		return t
+	}
+	cfg := config.PreSignedURL{
+		AllowedHTTPMethods: []string{"get"},
+		Enabled:            true,
+	}
+	store := MyStoreService{}
+	pua := SignedURLAuthenticator{
+		PreSignedURLConfig: cfg,
+		Store:              store,
+		Now:                nowFunc,
+	}
+
+	tests := []struct {
+		now          string
+		url          string
+		errorMessage string
+	}{
+		{"2020-02-02T12:30:00.000Z", "http://example.com/example.jpg?OC-Date=2020-02-02T12:29:00.000Z&OC-Expires=invalid", "required OC-Signature parameter not found"},
+		{"2020-02-02T12:30:00.000Z", "http://cloud.example.net/?OC-Credential=alice&OC-Date=2019-05-14T11%3A01%3A58.135Z&OC-Expires=1200&OC-Verb=GET&OC-Signature=f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b6", "URL is expired"},
+		{"2019-05-14T11:02:00.000Z", "http://cloud.example.net/?OC-Credential=alice&OC-Date=2019-05-14T11%3A01%3A58.135Z&OC-Expires=1200&OC-Verb=GET&OC-Signature=f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b", "signature mismatch: expected f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b6 != actual f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b"},
+		{"2019-05-14T11:02:00.000Z", "http://cloud.example.net/?OC-Credential=alice&OC-Date=2019-05-14T11%3A01%3A58.135Z&OC-Expires=1200&OC-Verb=GET&OC-Signature=f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b6", ""},
+		{"2019-05-14T11:02:00.000Z", "http://cloud.example.net/?OC-Date=2019-05-14T11%3A01%3A58.135Z&OC-Expires=1200&OC-Verb=GET&OC-Credential=alice&OC-Signature=f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b6", ""},
+		{"2019-05-14T11:02:00.000Z", "http://cloud.example.net/?OC-Algo=PBKDF2%2F10000-SHA512&OC-Date=2019-05-14T11%3A01%3A58.135Z&OC-Expires=1200&OC-Verb=GET&OC-Credential=alice&OC-Signature=f9e53a1ee23caef10f72ec392c1b537317491b687bfdd224c782be197d9ca2b6", ""},
+		{"2024-02-07T12:03:11.966Z", "http://localhost:33001/try?id=1&id=2&OC-Credential=user&OC-Date=2024-02-07T12%3A03%3A11.966Z&OC-Expires=2&OC-Verb=GET&OC-Algo=PBKDF2%2F10000-SHA512&OC-Signature=86e21a1efbf0be989a206109cfedf70a22f338dc8995e849ce002032bc6741c5", ""},
+	}
+
+	for _, tt := range tests {
+		u := userpb.User{
+			Id:          &userpb.UserId{OpaqueId: "useri"},
+			DisplayName: "Test User",
+		}
+		ctx := revactx.ContextSetUser(context.Background(), &u)
+
+		pua.Now = func() time.Time {
+			t, _ := time.Parse(time.RFC3339, tt.now)
+			return t
+		}
+
+		r := httptest.NewRequest("", tt.url, nil).WithContext(ctx)
+		err := pua.validate(r)
+		if tt.errorMessage == "" {
+			assert.Nil(t, err)
+		} else {
+			assert.EqualError(t, err, tt.errorMessage)
+		}
 	}
 }
