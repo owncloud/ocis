73652ffd96acdc28004dd496e04a8275b8ab496a
Christian Richter <crichter@owncloud.com>
2024-02-13 13:25:52 +0100

    add cli options to main.go
    Signed-off-by: Christian Richter <crichter@owncloud.com>

---
+++ docs/helpers/envvardeltas.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func RenderEnvVarDeltas() {
+	fmt.Println("RenderEnvVarDeltas")
+}
--- docs/helpers/main.go
+++ docs/helpers/main.go
@@ -1,8 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	RenderTemplates()
-	GetRogueEnvs()
-	RenderGlobalVarsTemplate()
-	GenerateServiceIndexMarkdowns()
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "templates":
+			RenderTemplates()
+		case "rogue":
+			GetRogueEnvs()
+		case "globals":
+			RenderGlobalVarsTemplate()
+		case "service-index":
+			GenerateServiceIndexMarkdowns()
+		case "envvar-deltas":
+			RenderEnvVarDeltas()
+		case "all":
+			RenderTemplates()
+			GetRogueEnvs()
+			RenderGlobalVarsTemplate()
+			GenerateServiceIndexMarkdowns()
+			RenderEnvVarDeltas()
+		case "help":
+			fallthrough
+		default:
+			fmt.Println("Usage: [templates|rogue|globals|service-index|envvar-deltas|all]")
+		}
+	} else {
+		// Left here, even though present in the switch case, for backwards compatibility
+		RenderTemplates()
+		GetRogueEnvs()
+		RenderGlobalVarsTemplate()
+		GenerateServiceIndexMarkdowns()
+		RenderEnvVarDeltas()
+	}
 }
