2ca57fdc20f5bb6f0c5b72326817081565a2aa7b
Christian Richter <crichter@owncloud.com>
2024-02-14 17:13:02 +0100

    add delta markdown generator
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- docs/helpers/main.go
+++ docs/helpers/main.go
@@ -16,6 +16,15 @@ func main() {
 			RenderGlobalVarsTemplate()
 		case "service-index":
 			GenerateServiceIndexMarkdowns()
+		case "env-var-delta-table":
+			// This step is not covered by the all or default case, because it needs explicit arguments
+			if len(os.Args) != 4 {
+				fmt.Println("Needs two arguments: env-var-delta-table <first-version> <second-version>")
+				fmt.Println("Example: env-var-delta-table v5.0.0 v6.0.0")
+				fmt.Println("Will not generate usable results vor versions Prior to v5.0.0")
+			} else {
+				RenderEnvVarDeltaTable(os.Args)
+			}
 		case "all":
 			RenderTemplates()
 			GetRogueEnvs()
@@ -24,7 +33,7 @@ func main() {
 		case "help":
 			fallthrough
 		default:
-			fmt.Println("Usage: [templates|rogue|globals|service-index|all]")
+			fmt.Println("Usage: env-var-delta-table [templates|rogue|globals|service-index|env-var-delta-table|all|help]")
 		}
 	} else {
 		// Left here, even though present in the switch case, for backwards compatibility
