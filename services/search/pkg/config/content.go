484daeac50b03bf4e41ec83a2be28f77fb7df43d
Phil Davis <phil@jankaritech.com>
2024-03-05 16:45:25 +0545

    chore: set introductionVersion 5.0 in services/search

--- services/search/pkg/config/content.go
+++ services/search/pkg/config/content.go
@@ -10,5 +10,5 @@ type Extractor struct {
 // ExtractorTika configures the Tika extractor
 type ExtractorTika struct {
 	TikaURL        string `yaml:"tika_url" env:"SEARCH_EXTRACTOR_TIKA_TIKA_URL" desc:"URL of the tika server." introductionVersion:"pre5.0"`
-	CleanStopWords bool   `yaml:"clean_stop_words" env:"SEARCH_EXTRACTOR_TIKA_CLEAN_STOP_WORDS" desc:"Defines if stop words should be cleaned or not. See the documentation for more details." introductionVersion:"pre5.0"`
+	CleanStopWords bool   `yaml:"clean_stop_words" env:"SEARCH_EXTRACTOR_TIKA_CLEAN_STOP_WORDS" desc:"Defines if stop words should be cleaned or not. See the documentation for more details." introductionVersion:"5.0"`
 }
