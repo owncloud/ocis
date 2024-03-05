1714eacbf0d6ac93f66cf1eb384df692fbf444cb
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2024-03-05 11:39:03 +0100

    docs: add the "introductionVersion" keyword in all env vars

--- services/collaboration/pkg/config/cs3api.go
+++ services/collaboration/pkg/config/cs3api.go
@@ -7,9 +7,9 @@ type CS3Api struct {
 }
 
 type Gateway struct {
-	Name string `yaml: "name" env:"OCIS_REVA_GATEWAY;COLLABORATION_CS3API_GATEWAY_NAME" desc:"The service name of the CS3API gateway."`
+	Name string `yaml: "name" env:"OCIS_REVA_GATEWAY;COLLABORATION_CS3API_GATEWAY_NAME" desc:"The service name of the CS3API gateway." introductionVersion:"5.1"`
 }
 
 type DataGateway struct {
-	Insecure bool `yaml:"insecure" env:"COLLABORATION_CS3API_DATAGATEWAY_INSECURE" desc:"Connect to the CS3API data gateway insecurely."`
+	Insecure bool `yaml:"insecure" env:"COLLABORATION_CS3API_DATAGATEWAY_INSECURE" desc:"Connect to the CS3API data gateway insecurely." introductionVersion:"5.1"`
 }
