f8f864e566ba25206f7617bad0ee2080c2a7f8be
Jörn Friedrich Dreyer <jfd@butonic.de>
2024-04-24 10:35:57 +0200

    always initialize http handler
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- services/proxy/pkg/staticroutes/oidc_well-known.go
+++ services/proxy/pkg/staticroutes/oidc_well-known.go
@@ -3,6 +3,8 @@ package staticroutes
 import (
 	"io"
 	"net/http"
+	"net/url"
+	"path"
 )
 
 var (
@@ -10,29 +12,33 @@ var (
 )
 
 // OIDCWellKnownRewrite is a handler that rewrites the /.well-known/openid-configuration endpoint for external IDPs.
-func (s *StaticRouteHandler) oIDCWellKnownRewrite(w http.ResponseWriter, r *http.Request) {
-	wellKnownRes, err := s.OidcHttpClient.Get(s.oidcURL.String())
-	if err != nil {
-		s.Logger.Error().
-			Err(err).
-			Str("handler", "oidc wellknown rewrite").
-			Str("url", s.oidcURL.String()).
-			Msg("get information from url failed")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+func (s *StaticRouteHandler) oIDCWellKnownRewrite(issuer string) http.HandlerFunc {
+	oidcURL, _ := url.Parse(issuer)
+	oidcURL.Path = path.Join(oidcURL.Path, wellKnownPath)
+	return func(w http.ResponseWriter, r *http.Request) {
+		wellKnownRes, err := s.OidcHttpClient.Get(oidcURL.String())
+		if err != nil {
+			s.Logger.Error().
+				Err(err).
+				Str("handler", "oidc wellknown rewrite").
+				Str("url", oidcURL.String()).
+				Msg("get information from url failed")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	defer wellKnownRes.Body.Close()
+		defer wellKnownRes.Body.Close()
 
-	copyHeader(w.Header(), wellKnownRes.Header)
-	w.WriteHeader(wellKnownRes.StatusCode)
-	_, err = io.Copy(w, wellKnownRes.Body)
-	if err != nil {
-		s.Logger.Error().
-			Err(err).
-			Str("handler", "oidc wellknown rewrite").
-			Msg("copying response body failed")
+		copyHeader(w.Header(), wellKnownRes.Header)
+		w.WriteHeader(wellKnownRes.StatusCode)
+		_, err = io.Copy(w, wellKnownRes.Body)
+		if err != nil {
+			s.Logger.Error().
+				Err(err).
+				Str("handler", "oidc wellknown rewrite").
+				Msg("copying response body failed")
 
+		}
 	}
 }
 
