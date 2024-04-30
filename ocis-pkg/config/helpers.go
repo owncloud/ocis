c039041a406d21b85230062c41792b4f3c6e02fe
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2024-04-30 17:35:46 +0200

    test: unit test BindSourcesToStructs (#9029)

--- ocis-pkg/config/helpers.go
+++ ocis-pkg/config/helpers.go
@@ -1,16 +1,17 @@
 package config
 
 import (
-	"path"
-
 	gofig "github.com/gookit/config/v2"
 	gooyaml "github.com/gookit/config/v2/yaml"
-
 	"github.com/owncloud/ocis/v2/ocis-pkg/config/defaults"
+	"io/fs"
+	"os"
+	"path"
+	"strings"
 )
 
 var (
-	// decoderConfigTagname sets the tag name to be used from the config structs
+	// decoderConfigTagName sets the tag name to be used from the config structs
 	// currently we only support "yaml" because we only support config loading
 	// from yaml files and the yaml parser has no simple way to set a custom tag name to use
 	decoderConfigTagName = "yaml"
@@ -19,6 +20,12 @@ var (
 // BindSourcesToStructs assigns any config value from a config file / env variable to struct `dst`. Its only purpose
 // is to solely modify `dst`, not dealing with the config structs; and do so in a thread safe manner.
 func BindSourcesToStructs(service string, dst interface{}) (*gofig.Config, error) {
+	fileSystem := os.DirFS("/")
+	filePath := strings.TrimLeft(path.Join(defaults.BaseConfigPath(), service+".yaml"), "/")
+	return bindSourcesToStructs(fileSystem, filePath, service, dst)
+}
+
+func bindSourcesToStructs(fileSystem fs.FS, filePath, service string, dst interface{}) (*gofig.Config, error) {
 	cnf := gofig.NewWithOptions(service)
 	cnf.WithOptions(func(options *gofig.Options) {
 		options.ParseEnv = true
@@ -26,9 +33,17 @@ func BindSourcesToStructs(service string, dst interface{}) (*gofig.Config, error
 	})
 	cnf.AddDriver(gooyaml.Driver)
 
-	_ = cnf.LoadFiles(path.Join(defaults.BaseConfigPath(), service+".yaml"))
+	yamlContent, err := fs.ReadFile(fileSystem, filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return cnf, nil
+		}
 
-	err := cnf.BindStruct("", &dst)
+		return nil, err
+	}
+	_ = cnf.LoadSources("yaml", yamlContent)
+
+	err = cnf.BindStruct("", &dst)
 	if err != nil {
 		return nil, err
 	}
