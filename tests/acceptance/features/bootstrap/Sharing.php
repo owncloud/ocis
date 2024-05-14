54d9b5636e64467d16865698dabb2cd8fc497fc2
Salipa-Gurung <salipagurung@gmail.com>
2024-05-14 12:06:28 +0545

    update given step using sharingNg in accessToShare.feature

--- tests/acceptance/features/bootstrap/Sharing.php
+++ tests/acceptance/features/bootstrap/Sharing.php
@@ -2254,7 +2254,7 @@ trait Sharing {
 	 * @throws Exception
 	 */
 	public function checkingLastShareIDIsIncluded():void {
-		$shareId = $this->getLastCreatedUserGroupShareId();
+		$shareId = ($this->isUsingSharingNG()) ? $this->shareNgGetLastCreatedUserGroupShareID() : $this->getLastCreatedUserGroupShareId();
 		if (!$this->isFieldInResponse('id', $shareId)) {
 			Assert::fail(
 				"Share id $shareId not found in response"
@@ -2269,7 +2269,7 @@ trait Sharing {
 	 * @throws Exception
 	 */
 	public function checkLastShareIDIsNotIncluded():void {
-		$shareId = $this->getLastCreatedUserGroupShareId();
+		$shareId = $this->isUsingSharingNG() ? $this->shareNgGetLastCreatedUserGroupShareID() : $this->getLastCreatedUserGroupShareId();
 		if ($this->isFieldInResponse('id', $shareId, false)) {
 			Assert::fail(
 				"Share id $shareId has been found in response"
