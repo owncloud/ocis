efe4895a981fe4524f5c7c35a8dde35a6fb1911b
Michael Barz <mbarz@owncloud.com>
2024-06-04 11:06:44 +0200

    chore: update validation script for env var annotations

--- .make/check-env-var-annotations.sh
+++ .make/check-env-var-annotations.sh
@@ -5,6 +5,7 @@
 #
 # valid examples:
 #
+# introductionVersion:"%%NEXT%%"
 # introductionVersion:"pre5.0"
 # introductionVersion:"5.0"
 # introductionVersion:"4.9.3-rc5"
@@ -24,7 +25,7 @@ ERROR=0
 
 SEMVER_REGEX="([0-9]|[1-9][0-9]*)(\.([0-9]|[1-9][0-9]*)){1,2}(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?"
 
-QUERY_INTRO=$(git grep -n "env:" -- '*.go' |grep -v -P "introductionVersion:\"($SEMVER_REGEX|(pre5\.0))\""|grep -v "_test.go"|grep -v "vendor/")
+QUERY_INTRO=$(git grep -n "env:" -- '*.go' |grep -v -P "introductionVersion:\"($SEMVER_REGEX|(pre5\.0)|(%%NEXT%%))\""|grep -v "_test.go"|grep -v "vendor/")
 RESULTS_INTRO=$(echo "${QUERY_INTRO}"|wc -l)
 if [ "${QUERY_INTRO}" != "" ] && [ "${RESULTS_INTRO}" -gt 0 ]; then
   echo "==============================================================================================="
