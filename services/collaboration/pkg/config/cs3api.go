672ec3811c997f02143247f7f4307d25fc079f14
jkoberg <jkoberg@owncloud.com>
2024-06-24 11:53:35 +0200

    feat(ocis): deprecate gateway envvars
    Signed-off-by: jkoberg <jkoberg@owncloud.com>

--- services/collaboration/pkg/config/cs3api.go
+++ services/collaboration/pkg/config/cs3api.go
@@ -8,7 +8,7 @@ type CS3Api struct {
 
 // Gateway defines the available configuration for the CS3 API gateway
 type Gateway struct {
-	Name string `yaml:"name" env:"OCIS_REVA_GATEWAY;COLLABORATION_CS3API_GATEWAY_NAME" desc:"The service name of the CS3API gateway." introductionVersion:"6.0.0"`
+	Name string `yaml:"name" env:"OCIS_REVA_GATEWAY;COLLABORATION_CS3API_GATEWAY_NAME" desc:"The service name of the CS3API gateway." introductionVersion:"6.0.0" deprecationVersion:"6.0" removalVersion:"%%NEXT%%" deprecationInfo:"COLLABORATION_CS3API_GATEWAY_NAME removed from simplicity."`
 }
 
 // DataGateway defines the available configuration for the CS3 API data gateway
