03744875d8fab255ece567360cdac1ed4c9992ea
Michael Barz <mbarz@owncloud.com>
2024-07-08 09:49:16 +0200

    fix: add missing global env var

--- services/collaboration/pkg/config/grpc.go
+++ services/collaboration/pkg/config/grpc.go
@@ -2,6 +2,6 @@ package config
 
 // GRPC defines the available grpc configuration.
 type GRPC struct {
-	Addr      string `yaml:"addr" env:"COLLABORATION_GRPC_ADDR" desc:"The bind address of the GRPC service." introductionVersion:"6.0.0"`
+	Addr      string `yaml:"addr" env:"OCIS_GATEWAY_GRPC_ADDR;COLLABORATION_GRPC_ADDR" desc:"The bind address of the GRPC service." introductionVersion:"6.0.0"`
 	Namespace string `yaml:"-"`
 }
