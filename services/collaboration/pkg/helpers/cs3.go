9e7d570c794cb1f133ffbfba3b23e95f118695b4
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2024-08-13 15:04:19 +0200

    fix: verify file id in URL matches the one inside the access token
    We're using the file id from the access token, so changing the file id
    in the URL shouldn't matter. However, the file id must represent a
    single file.
    It will also help to detect unwanted changes in the URL.

--- services/collaboration/pkg/helpers/cs3.go
+++ services/collaboration/pkg/helpers/cs3.go
@@ -1,8 +1,13 @@
 package helpers
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+
 	gatewayv1beta1 "github.com/cs3org/go-cs3apis/cs3/gateway/v1beta1"
+	providerv1beta1 "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
 	"github.com/cs3org/reva/v2/pkg/rgrpc/todo/pool"
+	"github.com/cs3org/reva/v2/pkg/storagespace"
 	"github.com/owncloud/ocis/v2/services/collaboration/pkg/config"
 )
 
@@ -24,4 +29,12 @@ func GetCS3apiClient(cfg *config.Config, forceNew bool) (gatewayv1beta1.GatewayA
 		commonCS3ApiClient = client
 	}
 	return client, err
+}
+
+// HashResourceId builds a urlsafe and stable file reference that can be used for proxy routing,
+// so that all sessions on one file end on the same office server
+func HashResourceId(resourceId *providerv1beta1.ResourceId) string {
+	c := sha256.New()
+	c.Write([]byte(storagespace.FormatResourceID(resourceId)))
+	return hex.EncodeToString(c.Sum(nil))
 }
