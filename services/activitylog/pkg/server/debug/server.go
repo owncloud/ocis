f64ccd0d35911e77bed9a97b48f6f148f3af9b4d
Christian Richter <crichter@owncloud.com>
2024-10-10 11:12:41 +0200

    refactor checks for activitylog and antivirus
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- services/activitylog/pkg/server/debug/server.go
+++ services/activitylog/pkg/server/debug/server.go
@@ -1,11 +1,8 @@
 package debug
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
-	"github.com/cs3org/reva/v2/pkg/events/stream"
 	"github.com/owncloud/ocis/v2/ocis-pkg/handlers"
 	"github.com/owncloud/ocis/v2/ocis-pkg/service/debug"
 	"github.com/owncloud/ocis/v2/ocis-pkg/version"
@@ -17,13 +14,8 @@ func Server(opts ...Option) (*http.Server, error) {
 
 	checkHandler := handlers.NewCheckHandler(
 		handlers.NewCheckHandlerConfiguration().
-			WithLogger(options.Logger).WithCheck("nats reachability", func(ctx context.Context) error {
-			_, err := stream.NatsFromConfig("healthcheckfornats", false, stream.NatsConfig(options.Config.Events))
-			if err != nil {
-				return fmt.Errorf("could not connect to nats server: %v", err)
-			}
-			return nil
-		}),
+			WithLogger(options.Logger).
+			WithCheck("nats reachability", handlers.NewNatsCheck(options.Config.Events.Cluster)),
 	)
 
 	return debug.NewService(
