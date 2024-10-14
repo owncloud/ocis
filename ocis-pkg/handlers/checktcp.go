6064386787cc55d571cc777b4f04c2d2e4d8a6ca
Christian Richter <crichter@owncloud.com>
2024-10-14 08:22:47 +0200

    add timeout to tcp checker
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- ocis-pkg/handlers/checktcp.go
+++ ocis-pkg/handlers/checktcp.go
@@ -3,12 +3,13 @@ package handlers
 import (
 	"context"
 	"net"
+	"time"
 )
 
 // NewTCPCheck returns a check that connects to a given tcp endpoint.
 func NewTCPCheck(address string) func(ctx context.Context) error {
 	return func(_ context.Context) error {
-		conn, err := net.Dial("tcp", address)
+		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 		if err != nil {
 			return err
 		}
