dd59a36344b9e6d122adddbee77428fcf6930916
Christian Richter <crichter@owncloud.com>
2024-10-15 14:30:20 +0200

    fix bug in checkhttp.go
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- ocis-pkg/handlers/checkhttp.go
+++ ocis-pkg/handlers/checkhttp.go
@@ -13,10 +13,11 @@ func NewHTTPCheck(url string) func(context.Context) error {
 		c := http.Client{
 			Timeout: 3 * time.Second,
 		}
-		_, err := c.Get(url)
+		resp, err := c.Get(url)
 		if err != nil {
 			return fmt.Errorf("could not connect to http server: %v", err)
 		}
+		_ = resp.Body.Close()
 		return nil
 	}
 }
