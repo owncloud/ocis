0cb83312841ad779186cca0d5cbdb0ddb96f61d1
Christian Richter <crichter@owncloud.com>
2024-10-24 09:13:03 +0200

    avoid 0.0.0.0 & replace by outbound ip
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- ocis-pkg/checks/checkhttp.go
+++ ocis-pkg/checks/checkhttp.go
@@ -3,13 +3,27 @@ package checks
 import (
 	"context"
 	"fmt"
+	"github.com/owncloud/ocis/v2/ocis-pkg/handlers"
 	"net/http"
+	"strings"
 	"time"
 )
 
 // NewHTTPCheck checks the reachability of a http server.
 func NewHTTPCheck(url string) func(context.Context) error {
 	return func(_ context.Context) error {
+		if strings.Contains(url, "0.0.0.0") {
+			outboundIp, err := handlers.GetOutBoundIP()
+			if err != nil {
+				return err
+			}
+			url = strings.Replace(url, "0.0.0.0", outboundIp, 1)
+		}
+
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+
 		c := http.Client{
 			Timeout: 3 * time.Second,
 		}
