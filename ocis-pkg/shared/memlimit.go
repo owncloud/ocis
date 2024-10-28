b4f330278e411ece6e043d6a2671ee725ab9cf65
Roman Perekhod <rperekhod@owncloud.com>
2024-10-28 22:31:14 +0100

    set the memlimit default loglevel to error

--- ocis-pkg/shared/memlimit.go
+++ ocis-pkg/shared/memlimit.go
@@ -1,6 +1,15 @@
 package shared
 
 import (
-	// we import automemlimit here to include it for ocis als well as individual service binaries
-	_ "github.com/KimMachineGun/automemlimit"
+	"log/slog"
+
+	"github.com/KimMachineGun/automemlimit/memlimit"
 )
+
+// we init the memlimit here to include it for ocis als well as individual service binaries
+func init() {
+	slog.SetLogLoggerLevel(slog.LevelError)
+	_, _ = memlimit.SetGoMemLimitWithOpts(
+		memlimit.WithLogger(slog.Default()),
+	)
+}
