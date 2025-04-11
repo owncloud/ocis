74cf69b5ece6665ad183649197245e307d288d9d
Roman Perekhod <rperekhod@owncloud.com>
2025-04-11 09:41:15 +0200

    limited the length of tags

--- vendor/github.com/owncloud/reva/v2/pkg/tags/tags.go
+++ vendor/github.com/owncloud/reva/v2/pkg/tags/tags.go
@@ -19,7 +19,9 @@
 package tags
 
 import (
+	"fmt"
 	"strings"
+	"unicode/utf8"
 )
 
 var (
@@ -42,35 +44,45 @@ type Tags struct {
 // New creates a Tag struct from a slice of tags, e.g. ["tag1", "tag2"] or a list of tags, e.g. "tag1,tag2"
 func New(ts ...string) *Tags {
 	t := &Tags{sep: _tagsep, maxtags: _maxtags, exists: make(map[string]bool), t: make([]string, 0)}
-	t.addTags(ts)
+	t.addTags(t.normalize(ts))
 	return t
 }
 
 // Add appends a list of new tags and returns true if at least one was appended
 func (t *Tags) Add(ts ...string) bool {
-	return len(t.addTags(ts)) > 0
+	return len(t.addTags(t.normalize(ts))) > 0
+}
+
+// AddValidated appends a list of new tags and validates them using the provided validator function
+// It returns true if at least one was appended and an error if the validation failed
+func (t *Tags) AddValidated(validator func([]string) error, ts ...string) (bool, error) {
+	newTags := t.normalize(ts)
+	err := validator(newTags)
+	if err != nil {
+		return false, err
+	}
+	return len(t.addTags(newTags)) > 0, nil
 }
 
 // Remove removes a list of tags and returns true if at least one was removed
 func (t *Tags) Remove(s ...string) bool {
 	var removed bool
+	tags := t.normalize(s)
 
-	for _, tt := range s {
-		for _, tag := range strings.Split(tt, t.sep) {
-			if !t.exists[tag] {
-				continue
-			}
-
-			for i, tt := range t.t {
-				if tt == tag {
-					t.t = append(t.t[:i], t.t[i+1:]...)
-					break
-				}
-			}
-
-			delete(t.exists, tag)
-			removed = true
+	for _, tag := range tags {
+		if !t.exists[tag] {
+			continue
 		}
+
+		for i, tt := range t.t {
+			if tt == tag {
+				t.t = append(t.t[:i], t.t[i+1:]...)
+				break
+			}
+		}
+
+		delete(t.exists, tag)
+		removed = true
 	}
 	return removed
 }
@@ -86,31 +98,70 @@ func (t *Tags) AsSlice() []string {
 }
 
 // adds the tags and returns a list of added tags
+// the function receiving a normalized slice of tags, e.g.["tag1", "tag2"]
 func (t *Tags) addTags(s []string) []string {
-	added := make([]string, 0)
-	for _, tt := range s {
-		for _, tag := range strings.Split(tt, t.sep) {
-			if tag == "" {
-				// ignore empty tags
-				continue
-			}
-
-			if t.exists[tag] {
-				// tag is already existing
-				continue
-			}
-
-			if t.numtags >= t.maxtags {
-				// max number of tags reached. We return silently without warning anyone
-				break
-			}
-
-			added = append(added, tag)
-			t.exists[tag] = true
-			t.numtags++
+	added := make([]string, 0, len(t.t)+len(s))
+	for _, tag := range s {
+		if tag == "" {
+			// ignore empty tags
+			continue
 		}
+
+		if t.exists[tag] {
+			// tag is already existing
+			continue
+		}
+
+		if t.numtags >= t.maxtags {
+			// max number of tags reached. We return silently without warning anyone
+			break
+		}
+
+		added = append(added, tag)
+		t.exists[tag] = true
+		t.numtags++
 	}
 
 	t.t = append(added, t.t...)
 	return added
+}
+
+// normalize splits the tags and removes empty tags
+// the function receiving a slice of tags, e.g.["tag1", "tag2"] or a list of tags, e.g."tag1,tag2" or mixed.
+func (t *Tags) normalize(s []string) []string {
+	res := make([]string, 0, t.maxtags/2)
+	for _, tt := range s {
+		for _, tag := range strings.Split(tt, t.sep) {
+			ttr := strings.TrimSpace(tag)
+			if ttr == "" {
+				// ignore empty tags
+				continue
+			}
+			res = append(res, ttr)
+		}
+	}
+	return res
+}
+
+// MaxLengthValidator returns a function that validates the length of each tag in a slice
+func MaxLengthValidator(maxTagLength int) func([]string) error {
+	if maxTagLength <= 0 {
+		return func(tags []string) error { return nil }
+	}
+	return func(tags []string) error {
+		t := make([]string, 0, maxTagLength)
+		for _, tag := range tags {
+			if !utf8.ValidString(tag) {
+				return fmt.Errorf("tag [%s] contains invalid characters", tag)
+			}
+			if utf8.RuneCount([]byte(tag)) > maxTagLength {
+				t = append(t, tag)
+				continue
+			}
+		}
+		if len(t) > 0 {
+			return fmt.Errorf("tag [%s] too long, max length is %d", strings.Join(t, ", "), maxTagLength)
+		}
+		return nil
+	}
 }
