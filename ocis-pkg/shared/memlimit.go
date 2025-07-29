9b7470cd185dcef7730da8dd40a458489b8e6c34
Michal Klos <michal.klos@kiteworks.com>
2025-07-29 11:39:07 +0200

    fix: error GOMEMLIMIT in doc-generate

--- ocis-pkg/shared/memlimit.go
+++ ocis-pkg/shared/memlimit.go
@@ -2,6 +2,7 @@ package shared
 
 import (
 	"log/slog"
+	"os"
 
 	"github.com/KimMachineGun/automemlimit/memlimit"
 )
@@ -9,6 +10,11 @@ import (
 // we init the memlimit here to include it for ocis als well as individual service binaries
 func init() {
 	slog.SetLogLoggerLevel(slog.LevelError)
+
+	if os.Getenv("AUTOMEMLIMIT") == "off" {
+		return
+	}
+
 	_, _ = memlimit.SetGoMemLimitWithOpts(
 		memlimit.WithLogger(slog.Default()),
 	)
