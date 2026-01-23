f2c73cdc45478f5aa6fc4c3fc315e0eb503174fd
Julian Koberg <julian.koberg@kiteworks.com>
2026-01-23 19:00:43 +0100

    chore: release 8.0.0-rc.1
    Signed-off-by: Julian Koberg <julian.koberg@kiteworks.com>

--- ocis-pkg/version/version.go
+++ ocis-pkg/version/version.go
@@ -16,7 +16,7 @@ var (
 	// LatestTag is the latest released version plus the dev meta version.
 	// Will be overwritten by the release pipeline
 	// Needs a manual change for every tagged release
-	LatestTag = "7.3.1+dev"
+	LatestTag = "8.0.0-rc.1"
 
 	// Date indicates the build date.
 	// This has been removed, it looks like you can only replace static strings with recent go versions
