325c7d4c5a06a85439b55307202d15fee7f3f85e
Saw-jan <saw.jan.grg3e@gmail.com>
2026-03-16 15:04:36 +0545

    test: patch chart template only once
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- tests/config/k8s/setup.sh
+++ tests/config/k8s/setup.sh
@@ -20,26 +20,37 @@ CFG_DIR="$ROOT/tests/config/k8s"
 CHT_DIR="$CHART_REPO/charts/ocis"
 TPL_DIR="$CHT_DIR/templates"
 
-# patch ocis service templates
-for service in "$TPL_DIR"/*/; do
-    if [[ -f "$service/deployment.yaml" ]]; then
-        if grep -qE 'ocis.caEnv' "$service/deployment.yaml"; then
-            sed -i '/.*ocis.caEnv.*/a\{{- include "ocis.extraEnvs" . | nindent 12 }}' "$service/deployment.yaml"
-            sed -i '/.*ocis.caPath.*/a\{{- include "ocis.extraVolMounts" . | nindent 12 }}' "$service/deployment.yaml"
-            sed -i '/.*ocis.caVolume.*/a\{{- include "ocis.extraVolumes" . | nindent 8 }}' "$service/deployment.yaml"
-        else
-            sed -i '/env:/a\{{- include "ocis.extraEnvs" . | nindent 12 }}' "$service/deployment.yaml"
-            sed -i '/volumeMounts:/a\{{- include "ocis.extraVolMounts" . | nindent 12 }}' "$service/deployment.yaml"
-            sed -i '/volumes:/a\{{- include "ocis.extraVolumes" . | nindent 8 }}' "$service/deployment.yaml"
+function patch_templates(){
+    # patch ocis service templates
+    for service in "$TPL_DIR"/*/; do
+        if [[ -f "$service/deployment.yaml" ]]; then
+            if grep -qE 'ocis.caEnv' "$service/deployment.yaml"; then
+                sed -i '/.*ocis.caEnv.*/a\{{- include "ocis.extraEnvs" . | nindent 12 }}' "$service/deployment.yaml"
+                sed -i '/.*ocis.caPath.*/a\{{- include "ocis.extraVolMounts" . | nindent 12 }}' "$service/deployment.yaml"
+                sed -i '/.*ocis.caVolume.*/a\{{- include "ocis.extraVolumes" . | nindent 8 }}' "$service/deployment.yaml"
+            else
+                sed -i '/env:/a\{{- include "ocis.extraEnvs" . | nindent 12 }}' "$service/deployment.yaml"
+                sed -i '/volumeMounts:/a\{{- include "ocis.extraVolMounts" . | nindent 12 }}' "$service/deployment.yaml"
+                sed -i '/volumes:/a\{{- include "ocis.extraVolumes" . | nindent 8 }}' "$service/deployment.yaml"
+            fi
         fi
-    fi
-done
+    done
 
-# copy custom template resources
-cp -r $CFG_DIR/templates/* $TPL_DIR/
+    # copy custom template resources
+    cp -r $CFG_DIR/templates/* $TPL_DIR/
 
-# add authbasic service
-sed -i "/{{- define \"ocis.basicServiceTemplates\" -}}/a\  {{- \$_ := set .scope \"appNameAuthBasic\" \"authbasic\" -}}" $TPL_DIR/_common/_tplvalues.tpl
+    # add authbasic service
+    sed -i "/{{- define \"ocis.basicServiceTemplates\" -}}/a\  {{- \$_ := set .scope \"appNameAuthBasic\" \"authbasic\" -}}" $TPL_DIR/_common/_tplvalues.tpl
+
+    # [NOTE]
+    # Remove schema validation to add extra configs in values.yaml.
+    # Also this allows us to use fakeoffice as web-office server
+    rm "$CHT_DIR/values.schema.json"
+}
+
+if [[ -f "$CHT_DIR/values.schema.json" ]]; then
+    patch_templates
+fi
 
 if [[ "$ENABLE_ANTIVIRUS" == "true" ]]; then
     sed -i '/virusscan:/{n;s|false|true|}' $CFG_DIR/values.yaml
@@ -67,11 +78,6 @@ fi
 if [[ "$ENABLE_AUTH_APP" == "true" ]]; then
     sed -i '/authapp:/{n;s|false|true|}' $CFG_DIR/values.yaml
 fi
-
-# [NOTE]
-# Remove schema validation to add extra configs in values.yaml.
-# Also this allows us to use fakeoffice as web-office server
-rm "$CHT_DIR/values.schema.json"
 
 # copy custom values file
 cp $CFG_DIR/values.yaml "$CHT_DIR/ci/deployment-values.yaml"
