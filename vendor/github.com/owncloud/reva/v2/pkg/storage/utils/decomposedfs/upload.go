895d09db3fe8e6dc9bc30594e427823e274d8bcd
Michal Klos <michal.klos@kiteworks.com>
2026-08-14 09:41:31 +0200

    feat: [stable-8.2][OCISDEV-855] receivedsharecache: retry on transient storage errors in CAS loop (#12789)
    feat: [OCISDEV-855] receivedsharecache: retry on transient storage errors in CAS loop (#12621)

--- vendor/github.com/owncloud/reva/v2/pkg/storage/utils/decomposedfs/upload.go
+++ vendor/github.com/owncloud/reva/v2/pkg/storage/utils/decomposedfs/upload.go
@@ -136,6 +136,7 @@ func (fs *Decomposedfs) InitiateUpload(ctx context.Context, ref *provider.Refere
 	_, span := tracer.Start(ctx, "InitiateUpload")
 	defer span.End()
 	log := appctx.GetLogger(ctx)
+	log.Debug().Interface("ref", ref).Msg("decomposedfs:InitiateUpload:start")
 
 	// remember the path from the reference
 	refpath := ref.GetPath()
@@ -336,6 +337,7 @@ func (fs *Decomposedfs) InitiateUpload(ctx context.Context, ref *provider.Refere
 		}
 	}
 
+	log.Debug().Str("uploadid", session.ID()).Msg("decomposedfs:InitiateUpload:complete")
 	return map[string]string{
 		"simple": session.ID(),
 		"tus":    session.ID(),
